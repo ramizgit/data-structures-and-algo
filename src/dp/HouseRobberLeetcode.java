d1e4117b41968d7889a4690144b5b3121889beee
ramizgit <65121963+ramizgit@users.noreply.github.com>
2023-01-15 10:41:33 +0530

    Create HouseRobberLeetcode.java

---
+++ src/dp/HouseRobberLeetcode.java
@@ -0,0 +1,39 @@
+package dp;
+
+public class HouseRobberLeetcode {
+    public static void main(String[] args)
+    {
+        System.out.println(maxrob(new int[]{1,2,3,1}));//4
+
+        System.out.println(maxrob(new int[]{2,7,9,3,1}));//12
+
+        System.out.println(maxrob(new int[]{10,1,12,50}));//60
+    }
+
+    private static int maxrob(int[] arr)
+    {
+        if(arr == null || arr.length == 0){
+            return 0;
+        }
+
+        if(arr.length == 1){
+            return arr[0];
+        }
+        
+        if(arr.length == 2){
+            return Math.max(arr[0], arr[1]);
+        }
+
+        int max = 0;
+        int m1 = arr[0];
+        int m2 = Math.max(arr[0], arr[1]);
+
+        for(int i=2; i<arr.length; i++){
+            max = Math.max(arr[i] + m1, m2);
+            m1 = m2;
+            m2 = max;
+        }
+
+        return max;
+    }
+}
