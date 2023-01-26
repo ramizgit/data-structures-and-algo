b02ec821663a329526bcfaf1038e9e235cf2a21c
ramizgit <65121963+ramizgit@users.noreply.github.com>
2023-01-26 17:58:30 +0530

    Update DetectCycle.java

--- src/linkedlist/DetectCycle.java
+++ src/linkedlist/DetectCycle.java
@@ -4,35 +4,64 @@ public class DetectCycle {
 
     public static void main(String[] args)
     {
-        Node node5 = new Node(5, null);
+        Node node6 = new Node(6, null);
+        Node node5 = new Node(5, node6);
         Node node4 = new Node(4, node5);
         Node node3 = new Node(3, node4);
         Node node2 = new Node(2, node3);
         Node node1 = new Node(1, node2);
 
-        node5.setNext(node3);
+        node6.setNext(node3);
 
-        System.out.println(hasCycle(node1));
+        hasCycle(node1);
     }
 
-    private static boolean hasCycle(Node node)
+    private static boolean hasCycle(Node head)
     {
-        if(node == null){
+        if(head == null){
             return false;
         }
 
-        Node slow = node;
-        Node fast = node.next;
+        Node slow = head;
+        Node fast = head;
 
         while(fast != null && fast.next != null){
-            if(slow == fast){
-                return true;
-            }
-
             slow = slow.next;
             fast = fast.next.next;
+
+            if(slow == fast){
+                System.out.println("Cycle detected !!!");
+                System.out.println("length of cycle : "+ lengthOfCycle(fast, slow));
+                System.out.println("starting point of cycle : "+ startingPointOfCycle(head, slow).value);
+                return true;
+            }
         }
 
         return false;
+    }
+
+    private static int lengthOfCycle(Node fast, Node slow)
+    {
+        int length = 1;
+        fast = fast.next;
+
+        while(fast != slow){
+            fast = fast.next;
+            length++;
+        }
+
+        return length;
+    }
+
+    private static Node startingPointOfCycle(Node head, Node meet)
+    {
+        Node node = head;
+
+        while (node != meet){
+            node = node.next;
+            meet = meet.next;
+        }
+
+        return node;
     }
 }
