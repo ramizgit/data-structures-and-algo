26bf6e55c644a1f0e1115a8efe039c6ab4b45f9e
ramizgit <65121963+ramizgit@users.noreply.github.com>
2023-03-03 12:09:59 +0530

    Create LastStoneWeightLeetcode.java

---
+++ src/heap/LastStoneWeightLeetcode.java
@@ -0,0 +1,44 @@
+package heap;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class LastStoneWeightLeetcode {
+
+    public static void main(String[] args)
+    {
+        System.out.println(lastStoneWeight(new int[]{2,7,4,1,8,1})); //1
+        System.out.println(lastStoneWeight(new int[]{1})); //1
+        System.out.println(lastStoneWeight(new int[]{2,2})); //0
+        System.out.println(lastStoneWeight(new int[]{2,1,2})); //1
+        System.out.println(lastStoneWeight(new int[]{})); //0
+    }
+
+    private static int lastStoneWeight(int[] stones)
+    {
+        //initialize max heap
+        PriorityQueue<Integer> maxheap = new PriorityQueue<>(Collections.reverseOrder());
+
+        //add all stones to the max heap
+        for(int stone : stones){
+            maxheap.add(stone);
+        }
+
+        while (maxheap.size() > 1){
+            int haviestStone = maxheap.poll();
+            int secondHaviestStone = maxheap.poll();
+
+            int diff = haviestStone - secondHaviestStone;
+
+            if(diff != 0){
+                maxheap.add(diff);
+            }
+        }
+
+        if(maxheap.size() == 1){
+            return maxheap.poll();
+        }else {
+            return 0;
+        }
+    }
+}
