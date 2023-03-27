9896db92924932a9e8eb8d565795d6fa4504377f
ramizgit <65121963+ramizgit@users.noreply.github.com>
2023-03-27 19:30:50 +0530

    Update LastNumberInACircle.java

--- src/LastNumberInACircle.java
+++ src/LastNumberInACircle.java
@@ -1,19 +1,20 @@
-public class LastNumberInACircle {
+public class LastNumInACircle {
 
     public static void main(String[] args)
     {
-        lastNumberInCircle(5, 3);
+        //Josephus problem
+        System.out.println(lastnum(4, 3)); //1
+        System.out.println(lastnum(5, 3)); //4
+        System.out.println(lastnum(10, 3)); //4
     }
 
-    public static void lastNumberInCircle(int n, int m)
+    private static int lastnum(int n, int m)
     {
         int last = 0;
 
-        for(int i=2; i <= n; i++)
-        {
+        for(int i=2; i<=n; i++){
             last = (last + m) % i;
         }
-
-        System.out.println(last);
+        return last+1;
     }
 }
