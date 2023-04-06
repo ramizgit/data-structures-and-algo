af12eb17fb92dafce31d77f93f1c750efc861acf
ramizgit <65121963+ramizgit@users.noreply.github.com>
2023-04-06 14:29:33 +0530

    Update NumberOfEnclaveDFS.java

--- src/matrix/NumberOfEnclaveDFS.java
+++ src/matrix/NumberOfEnclaveDFS.java
@@ -53,6 +53,6 @@ public class NumberOfEnclaveDFS {
 
     private static boolean onTheBorder(int rows, int cols, int i, int j)
     {
-        return ((i ==0 || i == rows -1) || (j ==0 || j == cols-1));
+        return ((i == 0 || i == rows -1) || (j == 0 || j == cols-1));
     }
 }
