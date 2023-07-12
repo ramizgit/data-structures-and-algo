5373183accb30b489c1e43b3d666dfb7ff881ffb
ramizgit <65121963+ramizgit@users.noreply.github.com>
2023-07-12 11:53:34 +0530

    Update Board.java

--- src/lld/snakeLadder/Board.java
+++ src/lld/snakeLadder/Board.java
@@ -1,1 +1,43 @@
+package lld.snakeladder;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class Board {
+
+    private int size;
+    private Map<Integer, Integer> snakes;
+    private Map<Integer, Integer> ladders;
+
+    public Board(int size) {
+        this.size = size;
+        this.snakes = new HashMap<>();
+        this.ladders = new HashMap<>();
+    }
+
+    public int move(int currentPos, int diceRoll)
+    {
+        int newPos = currentPos + diceRoll;
+
+        //check out of boundary
+        if(newPos > this.size){
+            return currentPos;
+        }
+
+        //check snake bite
+        if(this.snakes.containsKey(newPos)){
+            return this.snakes.get(newPos);
+        }
+
+        //check ladder ride
+        if (this.ladders.containsKey(newPos)){
+            return this.ladders.get(newPos);
+        }
+
+        return newPos;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
