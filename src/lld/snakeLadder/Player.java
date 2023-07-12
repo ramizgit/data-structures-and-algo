8a9b8b728d7dd6b4976e393c65c69437d123d95d
ramizgit <65121963+ramizgit@users.noreply.github.com>
2023-07-12 11:55:12 +0530

    Update Player.java

--- src/lld/snakeLadder/Player.java
+++ src/lld/snakeLadder/Player.java
@@ -1,1 +1,28 @@
+package lld.snakeladder;
 
+public class Player {
+
+    private String name;
+    private int currentPosition;
+
+    public Player(String name) {
+        this.name = name;
+        this.currentPosition = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(int currentPosition) {
+        this.currentPosition = currentPosition;
+    }
+}
