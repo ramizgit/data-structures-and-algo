d79baffd680f1d8af9eba06ef94ad850690a55da
ramizgit <65121963+ramizgit@users.noreply.github.com>
2023-07-13 13:36:41 +0530

    Update Crawler.java

--- src/lld/webcrawler/Crawler.java
+++ src/lld/webcrawler/Crawler.java
@@ -1,1 +1,19 @@
+package lld.webcrawler;
 
+public class Crawler implements Runnable{
+
+    private String url;
+
+    public Crawler(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public void run() {
+
+        //rest call to the url
+        //parse response
+
+
+    }
+}
