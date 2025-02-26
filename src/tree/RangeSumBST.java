7942d68f9a15b4c1d2f9699a441411ef5c64a7a8
ramizgit <65121963+ramizgit@users.noreply.github.com>
2025-02-26 13:40:47 +0530

    Update RangeSumBST.java

--- src/tree/RangeSumBST.java
+++ src/tree/RangeSumBST.java
@@ -1,1 +1,34 @@
+package meta;
 
+import tree.Node;
+
+public class RangeSumBST {
+
+    public static void main(String[] args)
+    {
+        Node node7 = new Node(18, null, null);
+        Node node5 = new Node(7, null, null);
+        Node node4 = new Node(3, null, null);
+        Node node3 = new Node(15, null, node7);
+        Node node2 = new Node(5, node4, node5);
+        Node node = new Node(10, node2, node3);
+
+        System.out.println(dfs(node, 7, 15));
+
+    }
+
+    private static int dfs(Node node, int low, int high)
+    {
+        if(node == null) {
+            return 0;
+        }
+
+        if(node.value > high){
+            return dfs(node.left, low, high);
+        }else if(node.value < low){
+            return dfs(node.right, low, high);
+        }
+
+        return node.value + dfs(node.left, low, high) + dfs(node.right, low, high);
+    }
+}
