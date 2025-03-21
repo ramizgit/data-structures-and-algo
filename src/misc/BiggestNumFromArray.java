0da3fffeab4654c7beaa5f35c67f098c33230617
ramizgit <65121963+ramizgit@users.noreply.github.com>
2025-03-21 09:16:34 +0530

    Update BiggestNumFromArray.java

--- src/misc/BiggestNumFromArray.java
+++ src/misc/BiggestNumFromArray.java
@@ -1,1 +1,37 @@
+package misc;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BiggestNumFromArray {
+
+    public static void main(String[] args)
+    {
+        int[] arr = {3, 30, 34, 5, 9};
+        System.out.println(biggestNum(arr));
+    }
+
+    private static String biggestNum(int[] arr)
+    {
+        if(arr == null || arr.length == 0){
+            return "";
+        }
+
+        //convert to List<String>
+        List<String> nums = new ArrayList<>();
+        for(int a : arr){
+            nums.add(Integer.toString(a));
+        }
+
+        //now sort srting using custom comapartor
+        Collections.sort(nums, (a, b) -> (b+a).compareTo(a+b));
+
+        StringBuilder sb = new StringBuilder();
+        for(String n : nums){
+            sb.append(n);
+        }
+
+        return sb.toString();
+    }
+}
