2ad96e239b1a261ee145d2bbf93406bf034e5847
ramizgit <65121963+ramizgit@users.noreply.github.com>
2025-06-03 13:51:42 +0530

    Update LongestSubStrWithoutRepeatingCh.java

--- src/sliding_window/LongestSubStrWithoutRepeatingCh.java
+++ src/sliding_window/LongestSubStrWithoutRepeatingCh.java
@@ -35,33 +35,4 @@ public class LongestSubStrWithoutRepeatingCh {
 
         return maxLen;
     }
-
-    /*private static int lengthOfLongestSubstring2(String s)
-    {
-        int left = 0;
-        int right = 0;
-        int len = 0;
-
-        Set<Character> set = new HashSet<>();
-
-        while (right < s.length()){
-
-            if(!set.contains(s.charAt(right))){
-                set.add(s.charAt(right));
-                right++;
-                len = Math.max(len, right - left);
-            }else {
-                while (left <= right){
-                    if(s.charAt(left) == s.charAt(right)){
-                        set.remove(s.charAt(left));
-                        left++;
-                        break;
-                    }
-                    left++;
-                }
-            }
-        }
-
-        return len;
-    }*/
 }
