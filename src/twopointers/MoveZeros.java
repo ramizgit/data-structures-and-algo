4dd5c5cdafdc4bd5c125dd015f8bfc8c5819483e
ramizgit <65121963+ramizgit@users.noreply.github.com>
2025-06-03 10:19:32 +0530

    Update MoveZeros.java

--- src/twopointers/MoveZeros.java
+++ src/twopointers/MoveZeros.java
@@ -1,1 +1,31 @@
+package twopointer;
 
+public class MoveZeros {
+    //https://leetcode.com/problems/move-zeroes/description/
+
+    public static void main(String[] args)
+    {
+        moveZeroes(new int[]{0,0,0,3,12});
+    }
+
+    private static void moveZeroes(int[] nums)
+    {
+        int left = 0;
+        int right = 0;
+
+        while (right < nums.length){
+            if(nums[right] != 0){
+                //swap with left
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+
+                left++;
+            }
+
+            right++;
+        }
+
+        System.out.println(nums);
+    }
+}
