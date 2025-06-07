e9699a23e03d4e5ea21867033d93647636a33ca4
ramizgit <65121963+ramizgit@users.noreply.github.com>
2025-06-07 17:05:29 +0530

    Update ValidateStackSeq.java

--- src/stack/ValidateStackSeq.java
+++ src/stack/ValidateStackSeq.java
@@ -1,1 +1,32 @@
+package stack;
+
+import java.util.Stack;
+
+public class ValidateStackSeq {
+
+    //https://leetcode.com/problems/validate-stack-sequences/description/
+
+    public static void main(String[] args)
+    {
+        System.out.println(validateStackSequences(new int[]{1,2,3,4,5}, new int[]{4,5,3,2,1})); //true
+        System.out.println(validateStackSequences(new int[]{1,2,3,4,5}, new int[]{4,3,5,1,2})); //false
+    }
+
+    private static boolean validateStackSequences(int[] pushed, int[] popped)
+    {
+        Stack<Integer> stack = new Stack<>();
+
+        int j = 0;
+        for(int i=0; i<pushed.length; i++){
+            stack.push(pushed[i]);
+
+            while (j < popped.length && !stack.empty() && popped[j] == stack.peek()){
+                stack.pop();
+                j++;
+            }
+        }
+
+        return stack.empty();
+    }
+}
 
