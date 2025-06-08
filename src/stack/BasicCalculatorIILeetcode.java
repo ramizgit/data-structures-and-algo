0826f6d91ddc8419f55bb54a6065d9524d965698
ramizgit <65121963+ramizgit@users.noreply.github.com>
2025-06-08 16:30:22 +0530

    Update BasicCalculatorIILeetcode.java

--- src/stack/BasicCalculatorIILeetcode.java
+++ src/stack/BasicCalculatorIILeetcode.java
@@ -2,70 +2,53 @@ package stack;
 
 import java.util.Stack;
 
-public class BasicCalculatorIILeetcode {
-    //https://leetcode.com/problems/basic-calculator-ii/
+public class BasicCalculatorII {
 
     public static void main(String[] args)
     {
         System.out.println(calculate("3+2*2")); //7
-        System.out.println(calculate("3/2")); //1
+        System.out.println(calculate("3-4+2*2")); //3
+        System.out.println(calculate("3-4+2/2")); //0
         System.out.println(calculate(" 3+5 / 2 ")); //5
-        System.out.println(calculate(" 12+10*20+30-10+6/3 ")); //234
-
     }
 
     private static int calculate(String s)
     {
         Stack<Integer> stack = new Stack<>();
-        int result = 0;
-        int i=0;
-        int num = 0;
-        char op = '+';
+        int ans = 0;
+        int curr = 0;
+        //int sign = 1;
+        char operator = '+';
 
-        while(i<s.length())
-        {
-            if (isDigit(s.charAt(i))){
-                num = num * 10 + (s.charAt(i) - '0');
-            }
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
 
-            if( (!isDigit(s.charAt(i)) && s.charAt(i) != ' ') || i == s.length()-1){
-                switch (op) {
-                    case '+' :
-                        stack.push(num);
-                        break;
-                    case '-' :
-                        stack.push(-1 * num);
-                        break;
-                    case '*' :
-                        stack.push(stack.pop() * num);
-                        break;
-                    case '/' :
-                        stack.push(stack.pop() / num);
-                        break;
-                    default:
-                        break;
+            if(Character.isDigit(ch)) {
+                //get all digits
+                curr = ch - '0';
+                while (i < s.length()-1 && Character.isDigit(s.charAt(i+1))){
+                    curr = curr * 10 + (s.charAt(i+1) - '0');
+                    i++;
                 }
 
-                //reset num
-                num = 0;
-                //reset operator
-                op = s.charAt(i);
+                if(operator == '+'){
+                    stack.push(curr);
+                } else if (operator == '-') {
+                    stack.push(-1 * curr);
+                } else if (operator == '*') {
+                    stack.push(stack.pop() * curr);
+                } else if (operator == '/') {
+                    stack.push(stack.pop() / curr);
+                }
+            }else {
+                operator = ch;
             }
-            i++;
         }
 
-        //now sum all elements of the stack
         while (!stack.empty()){
-            result += stack.pop();
+            ans += stack.pop();
         }
 
-        return result;
-    }
-
-    private static boolean isDigit(char ch)
-    {
-        int n = ch - '0';
-        return n >= 0 && n <= 9;
-        //return Character.isDigit(ch);
+        return ans;
     }
 }
