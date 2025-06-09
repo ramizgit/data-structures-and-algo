68c15b2bf80818b7291750310f9c3202445c028b
ramizgit <65121963+ramizgit@users.noreply.github.com>
2025-06-09 13:18:54 +0530

    Update RotatedSortedArray.java

--- src/binarysearch/RotatedSortedArray.java
+++ src/binarysearch/RotatedSortedArray.java
@@ -31,5 +31,38 @@ public class RotatedSortedArray {
 
         return -1;
     }
+
+     public static int findElement(int[] arr, int target)
+    {
+        int low = 0;
+        int high = arr.length - 1;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(arr[mid] == target){
+                //pivot element found
+                return mid;
+            }else {
+                if(arr[mid] < arr[high]){
+                    //we are in right sorted array
+                    if(target > arr[mid] &&  target <= arr[high]){
+                        low = mid + 1;
+                    }else {
+                        high = mid - 1;
+                    }
+                }else {
+                    //we are in left sorted array
+                    if(target >= arr[low] &&  target < arr[mid]){
+                        high = mid - 1;
+                    }else {
+                        low = mid + 1;
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
 }
 
