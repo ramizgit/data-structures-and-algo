05a6350b6e7f5a31f0e054e15fe8da6fcfccfefb
ramizgit <65121963+ramizgit@users.noreply.github.com>
2025-06-09 13:17:54 +0530

    Update RotatedSortedArray.java

--- src/binarysearch/RotatedSortedArray.java
+++ src/binarysearch/RotatedSortedArray.java
@@ -1,1 +1,35 @@
+package binarysearch;
+
+public class RotatedSortedArray {
+
+    public static void main(String[] args)
+    {
+        System.out.println(findPivotElement(new int[]{1,2,3}));
+        System.out.println(findPivotElement(new int[]{3,2,1}));
+        System.out.println(findPivotElement(new int[]{7,8,9,10,0,1,2,3,4,5,6}));
+    }
+
+    private static int findPivotElement(int[] arr)
+    {
+        int low = 0;
+        int high = arr.length-1;
+
+        while (low <= high){
+            int mid = low + (high - low)/2;
+
+            if( mid == 0 || mid == arr.length-1 || (arr[mid] < arr[mid-1] && arr[mid] < arr[mid+1]) ){
+                System.out.println("pivot idx : "+mid);
+                return arr[mid];
+            } else if (arr[mid] > arr[high]) {
+                //we are in left sorted, move right
+                low = mid + 1;
+            }else{
+                //we are in right sorted, move left
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
 
