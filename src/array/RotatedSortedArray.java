3ef1a4c741bd137b7b27fff60cd07ab7ce6926b7
ramizgit <65121963+ramizgit@users.noreply.github.com>
2025-06-09 13:19:15 +0530

    Update RotatedSortedArray.java

--- src/array/RotatedSortedArray.java
+++ src/array/RotatedSortedArray.java
@@ -19,25 +19,23 @@ public class RotatedSortedArray {
         System.out.println(findElement(new int[]{10,11,12,13,14,15,16,17,5,6}, 26)); //-1
     }
 
-    public static int getPivotElement(int[] arr)
+    private static int findPivotElement(int[] arr)
     {
         int low = 0;
-        int high = arr.length - 1;
-        int mid;
+        int high = arr.length-1;
 
-        while(low <= high){
-            mid = (low + high) >>> 1;
-            //mid = (high - low)/2 + low;
+        while (low <= high){
+            int mid = low + (high - low)/2;
 
-            if( (mid == arr.length-1) || (mid == 0) || (arr[mid] < arr[mid-1] && arr[mid] < arr[mid+1])){
-                //pivot element found
+            if( mid == 0 || mid == arr.length-1 || (arr[mid] < arr[mid-1] && arr[mid] < arr[mid+1]) ){
+                System.out.println("pivot idx : "+mid);
                 return arr[mid];
-            }else if(arr[mid] < arr[low] && arr[mid] < arr[high]){
-                //we are in right sorted array, reduce high pointer
-                high = mid - 1;
-            }else {
-                //we are in left sorted array, increase low pointer
+            } else if (arr[mid] > arr[high]) {
+                //we are in left sorted, move right
                 low = mid + 1;
+            }else{
+                //we are in right sorted, move left
+                high = mid - 1;
             }
         }
 
