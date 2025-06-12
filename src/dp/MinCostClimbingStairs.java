df126a05a394594e4d5f4ee965a44d65c1a38edf
ramizgit <65121963+ramizgit@users.noreply.github.com>
2025-06-12 18:41:41 +0530

    Create MinCostClimbingStairs.java

---
+++ src/dp/MinCostClimbingStairs.java
@@ -0,0 +1,27 @@
+package dp;
+
+public class MinCostClimbingStairs {
+
+    //https://leetcode.com/problems/min-cost-climbing-stairs/description/
+
+    public static void main(String[] args)
+    {
+        System.out.println(minCostClimbingStairs(new int[]{10,15,20})); //15
+        System.out.println(minCostClimbingStairs(new int[]{1,100,1,1,1,100,1,1,100,1})); //6
+    }
+
+    private static int minCostClimbingStairs(int[] cost)
+    {
+        int[] dp = new int[cost.length+1];
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for(int i=2; i<dp.length; i++){
+            int minOneCost = dp[i-1] + cost[i-1];
+            int minTwoCost = dp[i-2] + cost[i-2];
+            dp[i] = Math.min(minOneCost, minTwoCost);
+        }
+
+        return dp[dp.length-1];
+    }
+}
