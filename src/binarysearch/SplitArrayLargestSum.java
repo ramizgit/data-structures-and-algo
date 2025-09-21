39834ee0fa6de83bcdf5ea34f134add08f0eb0aa
ramizgit <65121963+ramizgit@users.noreply.github.com>
2025-09-21 12:51:23 +0530

    Update SplitArrayLargestSum.java

--- src/binarysearch/SplitArrayLargestSum.java
+++ src/binarysearch/SplitArrayLargestSum.java
@@ -1,1 +1,53 @@
+package binarysearch;
+
+public class SplitArrayLargestSum {
+    public static void main(String[] args)
+    {
+
+        //1,2,3,10
+
+    }
+
+    private static int splitArray(int[] nums, int k)
+    {
+        int left = 0;
+        int right = 0;
+        int answer = Integer.MAX_VALUE;
+
+        for(int i=0; i<nums.length; i++){
+            left = Math.max(left, nums[i]);
+            right += nums[i];
+        }
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+
+            if(canSplit(nums, k, mid)){
+                answer = Math.min(answer, mid);
+                right = mid - 1;
+            }else{
+               left = mid + 1;
+            }
+        }
+        return answer;
+    }
+
+    private static boolean canSplit(int[] nums, int k, int maxSum)
+    {
+        int sum = 0;
+        int count = 1;
+
+        for(int num : nums){
+            sum += num;
+
+            if(sum > maxSum){
+                count++;
+                //reset
+                sum = num;
+            }
+        }
+
+        return count <= k;
+    }
+}
 
