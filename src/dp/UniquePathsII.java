7032ceb72d1a2108e94e8128333b8e60ed47d3cd
ramizgit <65121963+ramizgit@users.noreply.github.com>
2025-09-29 21:02:50 +0530

    Update UniquePathsII.java

--- src/dp/UniquePathsII.java
+++ src/dp/UniquePathsII.java
@@ -8,37 +8,38 @@ public class UniquePathsII {
         System.out.println(uniquePaths(new int[][]{{0,1}, {0,0}})); //1
     }
 
-    private static int uniquePaths(int[][] obstacleGrid)
+    private static int uniquePaths(int[][] grid)
     {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
+
         int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0] == 1 ? 0 : 1;
 
-        //populate first column
-        for(int i=0; i<m; i++){
-            if(obstacleGrid[i][0] == 1){
+        //first row
+        for(int i=1; i<n; i++){
+            if(grid[0][i] == 1){
+                dp[0][i] = 0;
+            }else{
+                dp[0][i] = dp[0][i-1];
+            }
+        }
+
+        //first column
+        for(int i=1; i<m; i++){
+            if(grid[i][0] == 1){
                 dp[i][0] = 0;
-            }else {
-                dp[i][0] = 1;
-            }
-
-        }
-
-        //populate first row
-        for(int j=0; j<n; j++){
-            if(obstacleGrid[0][j] == 1){
-                dp[0][j] = 0;
-            }else {
-                dp[0][j] = 1;
+            }else{
+                dp[i][0] = dp[i-1][0];
             }
         }
 
-        //run dp
+        //rest of dp
         for(int i=1; i<m; i++){
             for(int j=1; j<n; j++){
-                if(obstacleGrid[i][j] == 1){
+                if(grid[i][j] == 1){
                     dp[i][j] = 0;
-                }else {
+                }else{
                     dp[i][j] = dp[i-1][j] + dp[i][j-1];
                 }
             }
@@ -47,4 +48,3 @@ public class UniquePathsII {
         return dp[m-1][n-1];
     }
 }
-
