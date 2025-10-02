bbd9a04d433416d64bdf0f689fd94e2148acc34a
ramizgit <65121963+ramizgit@users.noreply.github.com>
2025-10-02 10:48:08 +0530

    Update LongestCommonSubstring.java

--- src/dp/LongestCommonSubstring.java
+++ src/dp/LongestCommonSubstring.java
@@ -11,27 +11,17 @@ public class LongestCommonSubstring {
         int m = str1.length();
         int n = str2.length();
 
-        int[][] dp = new int[m+1][n+1];
+        int[][] dp = new int[m+1][n+1]; //initialized with all zero
         int maxCommonSubStrLength = 0;
         int maxIdxRow = 0;
         int maxIdxCol = 0;
-
-        //initialize first row to zero
-        for(int i = 0; i < n; i++){
-            dp[0][i] = 0;
-        }
-
-        //initialize first column to zero
-        for(int i = 0; i < m; i++){
-            dp[i][0] = 0;
-        }
 
         for(int i = 1; i <= m; i++)
         {
             for(int j = 1; j <= n; j++)
             {
                 if(str1.charAt(i-1) == str2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1] + 1;
+                    dp[i][j] = 1 + dp[i-1][j-1];
 
                     //update max length seen so far
                     if(dp[i][j] > maxCommonSubStrLength){
