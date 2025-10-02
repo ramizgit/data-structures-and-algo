2192ee7f9fdab99b0ffb936cb76a373d8d321b34
ramizgit <65121963+ramizgit@users.noreply.github.com>
2025-10-02 11:19:57 +0530

    Update LongestCommonSubsequence.java

--- src/dp/LongestCommonSubsequence.java
+++ src/dp/LongestCommonSubsequence.java
@@ -1,18 +1,19 @@
 package dp;
 
 public class LongestCommonSubsequence {
+
     public static void main(String[] args)
     {
-        findLongestCommonSubsequence("abcdefyz", "abcdghefxz"); //7
+        System.out.println(findLongestCommonSubsequence("abcde", "ace"));
+
     }
 
-    public static int findLongestCommonSubsequence(String s1, String s2)
+    private static int findLongestCommonSubsequence(String str1, String str2)
     {
         int m = str1.length();
         int n = str2.length();
 
         int[][] dp = new int[m+1][n+1];
-        int max = Integer.MIN_VALUE;
 
         for(int i=1; i<=m; i++){
             for(int j=1; j<=n; j++){
@@ -22,10 +23,8 @@ public class LongestCommonSubsequence {
                 }else{
                     dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
                 }
-
-                max = Math.max(max, dp[i][j]);
             }
         }
-        return max;
+        return dp[m][n];
     }
 }
