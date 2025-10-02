f34f73d12597b674490aefbe9a513dbcc5ae8f64
ramizgit <65121963+ramizgit@users.noreply.github.com>
2025-10-02 14:35:48 +0530

    Update DecodeWays.java

--- src/dp/DecodeWays.java
+++ src/dp/DecodeWays.java
@@ -2,39 +2,29 @@ package dp;
 
 public class DecodeWays {
     //https://leetcode.com/problems/decode-ways/description/
-
     public static void main(String[] args)
     {
-        System.out.println(numDecodings("06"));
-        System.out.println(numDecodings("1"));
         System.out.println(numDecodings("12"));
-        System.out.println(numDecodings("123"));
+        System.out.println(numDecodings("226"));
+        System.out.println(numDecodings("061"));
+        System.out.println(numDecodings("10011"));
     }
 
-    private static int numDecodings(String s)
+    private static int numDecodings(String input)
     {
-        int n = s.length();
+        int n = input.length();
         int[] dp = new int[n+1];
-
-        //empty string handling
         dp[0] = 1;
-
-        //first char handling
-        char ch = s.charAt(0);
-        if(ch == '0'){
-            return 0;
-        }
-        dp[1] = 1;
+        dp[1] = input.charAt(0) == '0' ? 0 : 1;
 
         for(int i=2; i<=n; i++){
-            int currOneDigit = Integer.valueOf(s.charAt(i-1));
-            int currTwoDigit = Integer.valueOf(s.substring(i-2, i));
-
-            if(currOneDigit >= 1){
+            int oneDigit = Integer.parseInt(input.substring(i-1, i)); //input.charAt(i-1) - '0';
+            if(oneDigit >= 1){
                 dp[i] = dp[i-1];
             }
 
-            if(currTwoDigit >= 10 && currTwoDigit <= 26){
+            int twoDigits = Integer.parseInt(input.substring(i-2, i));
+            if(twoDigits >= 10 && twoDigits <= 26){
                 dp[i] += dp[i-2];
             }
         }
