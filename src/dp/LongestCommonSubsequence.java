38a18a98f6c606dba15b6d06f5f42138fac716b3
ramizgit <65121963+ramizgit@users.noreply.github.com>
2025-10-02 11:11:49 +0530

    Update LongestCommonSubsequence.java

--- src/dp/LongestCommonSubsequence.java
+++ src/dp/LongestCommonSubsequence.java
@@ -6,40 +6,26 @@ public class LongestCommonSubsequence {
         findLongestCommonSubsequence("abcdefyz", "abcdghefxz"); //7
     }
 
-    public static void findLongestCommonSubsequence(String s1, String s2)
+    public static int findLongestCommonSubsequence(String s1, String s2)
     {
-        int row = s1.length()+1;
-        int col = s2.length()+1;
+        int m = str1.length();
+        int n = str2.length();
 
-        //dp matrix
-        int[][] dp = new int[row][col];
-        int maxCommonSubSeqLen = 0;
+        int[][] dp = new int[m+1][n+1];
+        int max = Integer.MIN_VALUE;
 
-        //set first row to zero
-        for(int i=0; i<col; i++){
-            dp[0][i] = 0;
-        }
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(str1.charAt(i-1) == str2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
 
-        //set first col to zero
-        for(int i=0; i<row; i++){
-            dp[i][0] = 0;
-        }
-
-        for(int i=1; i<row; i++){
-            for(int j=1; j<col; j++){
-                if(s1.charAt(i-1) == s2.charAt(j-1)){
-                    //if same, diagonal+1
-                    dp[i][j] = dp[i-1][j-1]+1;
-
-                    //update max len
-                    maxCommonSubSeqLen = Math.max(maxCommonSubSeqLen, dp[i][j]);
-                }else {
-                    //else, max of left or top neighbor
+                }else{
                     dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
                 }
+
+                max = Math.max(max, dp[i][j]);
             }
         }
-
-        System.out.println("maxCommonSubSeqLen : "+maxCommonSubSeqLen);
+        return max;
     }
 }
