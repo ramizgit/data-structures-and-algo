2ac680423375dc533e0b68bcb203dc48adc95d39
ramizgit <65121963+ramizgit@users.noreply.github.com>
2025-10-11 17:35:53 +0530

    Update WordSearch.java

--- src/matrix/WordSearch.java
+++ src/matrix/WordSearch.java
@@ -1,56 +1,59 @@
-package matrix;
+package grid;
 
 public class WordSearch {
-    //https://leetcode.com/problems/word-search/description/
 
     public static void main(String[] args)
     {
-        char[][] board = {{'A','B','C','E'}, {'S','F','C','S'}, {'A','D','E','E'}};
-        System.out.println(exist(board, "ABCCED")); //true
-        System.out.println(exist(board, "ABCCEDA")); //true
-        System.out.println(exist(board, "ABCCEDAZ")); //true
+        char[][] board = {{'A','B','C','E'},
+                          {'S','F','C','S'},
+                          {'A','D','E','E'}};
+        System.out.println(wordSearch(board, "ABCCED")); //true
+        System.out.println(wordSearch(board, "ABCCEDA")); //true
+        System.out.println(wordSearch(board, "ABCCEDAZ")); //false
     }
 
-    private static boolean exist(char[][] board, String word)
+    private static boolean wordSearch(char[][] grid, String word)
     {
-        int m = board.length;
-        int n = board[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
+
         boolean[][] visited = new boolean[m][n];
-        boolean result;
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(board[i][j] == word.charAt(0)){
-                    result = dfs(board, word, i, j, visited, 1, m, n);
-                    if(result){
+                if(grid[i][j] == word.charAt(0)){
+                    if(dfs(grid, visited, m, n, i, j, 1, word)){
                         return true;
                     }
                 }
             }
         }
-
         return false;
     }
-
-    private static boolean dfs(char[][] board, String word, int i, int j, boolean[][] visited, int len, int m, int n)
+    
+    private static boolean dfs(char[][] grid, boolean[][] visited, int m, int n, int i, int j, int len, String word)
     {
+        visited[i][j] = true;
+
         if(len == word.length()){
             return true;
         }
 
-        visited[i][j] = true;
-
-        int[][] directions = { {0, -1}, {0, 1}, {1, 0}, {-1, 0} };
+        int[][] directions = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
 
         for(int[] dir : directions){
             int x = i + dir[0];
             int y = j + dir[1];
 
-            if(x >= 0 && y >= 0 && x < m && y < n && !visited[x][y] && len < word.length() && board[x][y] == word.charAt(len)){
-                len++;
-                return dfs(board, word, x, y, visited, len, m, n);
+            if(x >= 0 && y >= 0 && x < m && y < n && !visited[x][y] && grid[x][y] == word.charAt(len)){
+                if(dfs(grid, visited, m, n, x, y, len + 1, word)){
+                    return true;
+                }
             }
         }
+
+        //backtrack
+        visited[i][j] = false;
         return false;
     }
 }
