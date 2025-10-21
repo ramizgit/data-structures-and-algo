b4c281a7d2c3aa9a329fa7bf1a5c1ec52ca58a33
ramizgit <65121963+ramizgit@users.noreply.github.com>
2025-10-21 08:24:54 +0530

    Update BasicCalculator.java

--- src/stack/BasicCalculator.java
+++ src/stack/BasicCalculator.java
@@ -1,55 +1,57 @@
 package stack;
 
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class BasicCalculator {
-
     public static void main(String[] args)
     {
         System.out.println(calculate("10+12-20+40")); //42
         System.out.println(calculate("(1+(4+5+2)-3)+(6+8)")); //23
         System.out.println(calculate("123")); //123
+        System.out.println(calculate("-2+3")); //123
     }
 
     private static int calculate(String s)
     {
-        Stack<Integer> stack = new Stack<>();
-        int curr = 0;
-        int ans = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        int result = 0;
         int sign = 1;
+        int num = 0;
 
         for(int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
+
             if(Character.isDigit(ch)){
                 //get all digits
-                curr = ch - '0';
-                while (i < s.length()-1 && Character.isDigit(s.charAt(i+1))){
-                    curr = curr * 10 + (s.charAt(i+1) - '0');
+                num = ch - '0';
+                while(i<s.length()-1 && Character.isDigit(s.charAt(i+1))){
+                    num = num * 10 + (s.charAt(i+1) - '0');
                     i++;
                 }
-                curr = curr * sign;
-                ans += curr;
-            } else if (ch == '+') {
+                num *= sign;
+                result += num;
+            }else if(ch == '+'){
                 sign = 1;
-            } else if (ch == '-') {
+            }else if(ch == '-'){
                 sign = -1;
-            } else if (ch == '(') {
-                //stack operation
-                stack.push(ans);
+            }else if(ch == '('){
+                //push into stack
+                stack.push(result);
                 stack.push(sign);
 
                 //reset
-                ans = 0;
+                result = 0;
                 sign = 1;
-            } else if (ch == ')') {
-                //stak operation
+            }else if(ch == ')'){
+                //add to previous result
                 int prevSign = stack.pop();
-                ans *= prevSign;
-                int prevAns = stack.pop();
-                ans += prevAns;
+                int prevResult = stack.pop();
+                result *= prevSign;
+                result += prevResult;
             }
         }
-        return ans;
+
+        return result;
     }
 }
-
