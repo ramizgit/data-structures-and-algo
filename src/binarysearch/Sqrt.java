cae238163a855f6d9ed3d9b0a41f72e95a75676d
ramizgit <65121963+ramizgit@users.noreply.github.com>
2025-10-24 17:49:02 +0530

    Update Sqrt.java

--- src/binarysearch/Sqrt.java
+++ src/binarysearch/Sqrt.java
@@ -29,4 +29,27 @@ public class Sqrt {
 
         return result;
     }
+
+    //******JUST ANOTHER WAY TO SOLVE*************
+    private static int suqareRoot(int num)
+    {
+        int left = 1;
+        int right = num;
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            int square = mid * mid;
+
+            if(square == num){
+                return mid;
+            }else if(square > num){
+                right = mid -1;
+            }else{
+                left = mid + 1;
+            }
+        }
+
+        return right; //for floor
+        //return left; //for ceiling
+    }
 }
