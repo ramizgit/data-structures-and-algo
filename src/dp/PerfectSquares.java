440387a3049c856d1409525c84e0a85da7a914b6
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-01-11 12:29:11 +0530

    Create PerfectSquares.java

---
+++ src/dp/PerfectSquares.java
@@ -0,0 +1,40 @@
+package dp;
+
+import java.util.Arrays;
+
+public class PerfectSquares {
+    //https://leetcode.com/problems/perfect-squares/description/
+
+    public static void main(String[] args)
+    {
+        System.out.println(numSquares(12));
+        System.out.println(numSquares(13));
+        System.out.println(numSquares(1));
+    }
+
+    private static int numSquares(int n)
+    {
+        //populate array of perfect squares
+        int[] squares = new int[n+1];
+        for(int i=0; i<=n; i++){
+            squares[i] = i * i;
+        }
+
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0; //base case
+
+        for(int i=1; i<=n; i++){
+            for(int s : squares){
+                if(s == 0){
+                    continue;
+                }
+                if(i >= s){
+                    dp[i] = Math.min(dp[i], 1 + dp[i-s]);
+                }
+            }
+        }
+
+        return dp[n] == Integer.MAX_VALUE ? -1 : dp[n];
+    }
+}
