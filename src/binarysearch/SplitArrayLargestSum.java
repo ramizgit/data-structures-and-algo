3ecd106381f50174da9536c88a3e4baba78e7e19
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-02-01 11:01:20 +0530

    Update SplitArrayLargestSum.java

--- src/binarysearch/SplitArrayLargestSum.java
+++ src/binarysearch/SplitArrayLargestSum.java
@@ -3,31 +3,29 @@ package binarysearch;
 public class SplitArrayLargestSum {
     public static void main(String[] args)
     {
-
         //nums = [7,2,5,10,8], k = 2
         System.out.println(splitArray(new int[]{7,2,5,10,8}, 2));
-
     }
 
     private static int splitArray(int[] nums, int k)
     {
-        int left = 0;
-        int right = 0;
+        int low = 0;
+        int high = 0;
         int answer = Integer.MAX_VALUE;
 
-        for(int i=0; i<nums.length; i++){
-            left = Math.max(left, nums[i]);
-            right += nums[i];
+        for(int n : nums){
+            low = Math.max(low, n);
+            high += n;
         }
 
-        while(left <= right){
-            int mid = left + (right - left)/2;
+        while(low <= high){
+            int mid = low + (high - low)/2;
 
             if(canSplit(nums, k, mid)){
-                answer = Math.min(answer, mid);
-                right = mid - 1;
+                answer = mid; //possible answer
+                high = mid - 1;
             }else{
-               left = mid + 1;
+               low = mid + 1;
             }
         }
         return answer;
@@ -37,18 +35,18 @@ public class SplitArrayLargestSum {
     private static boolean canSplit(int[] nums, int k, int maxSum)
     {
         int sum = 0;
-        int count = 1;
+        int subarray = 1;
 
-        for(int num : nums){
-            sum += num;
+        for(int n : nums){
+            sum += n;
 
             if(sum > maxSum){
-                count++;
+                subarray++;
                 //reset
-                sum = num;
+                sum = n;
             }
         }
 
-        return count <= k;
+        return subarray <= k;
     }
 }
