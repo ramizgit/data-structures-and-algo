27839b9a21c7a01fc08a807103af2ac7017ccd36
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-02-20 06:58:50 +0530

    Update MinimumWindowSubstring.java

--- src/sliding_window/MinimumWindowSubstring.java
+++ src/sliding_window/MinimumWindowSubstring.java
@@ -1,71 +1,58 @@
-package slidingwindow;
+package slidingWindow;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MinimumWindowSubstring {
 
-    //https://leetcode.com/problems/minimum-window-substring/description/
-
-    public static void main(String[] args)
+    public String minWindow(String s, String t)
     {
-        System.out.println(minWindow("ADOBECODEBANC", "ABC")); //BANC
-        System.out.println(minWindow("AAAAAADBCA","ABC")); //BCA
-    }
-
-    private static String minWindow(String s, String t)
-    {
-        //handle edge cases
-        if(s.isEmpty() || t.isEmpty() || s.length() < t.length()){
+        if (s == null || t == null || s.length() < t.length()) {
             return "";
         }
-        
-        //get freqency of target string
-        Map<Character, Integer> tmap = new HashMap<>();
+
+        Map<Character, Integer> freq = new HashMap<>();
         for(char ch : t.toCharArray()){
-            tmap.put(ch, tmap.getOrDefault(ch, 0) + 1);
+            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
         }
 
         int left = 0;
         int right = 0;
-        int matched = 0;
-        int minLen = Integer.MAX_VALUE;
-        int start = 0;
-        int end = 0;
+        int result = Integer.MAX_VALUE;
+        int remaining = t.length();
+        int bestStart = 0;
+        int bestEnd = 0;
 
-        while (right < s.length()){
-            char rightCh = s.charAt(right);
+        while(right < s.length()){
+            char ch = s.charAt(right);
 
-            //reduce tmap freq is matching char found
-            if(tmap.containsKey(rightCh)){
-                tmap.put(rightCh, tmap.getOrDefault(rightCh, 0) - 1);
-
-                if(tmap.get(rightCh) == 0){
-                    //one char fully matched, increase count
-                    matched++;
+            if(freq.containsKey(ch)){
+                if(freq.get(ch) > 0){
+                    remaining--;
                 }
+                freq.put(ch, freq.get(ch) - 1);
             }
 
-            //move left ptr if fully matched
-            while (tmap.keySet().size() == matched){
-                //capture min window len
-                if(minLen > (right - left + 1)){
-                    minLen = right - left + 1;
-                    start = left;
-                    end = right + 1;
+            while(remaining == 0){
+                if(result > (right - left + 1)){
+                    result = right - left + 1;
+                    bestStart = left;
+                    bestEnd = right;
                 }
 
-                char leftCh = s.charAt(left++);
-                if(tmap.containsKey(leftCh)){
-                    if(tmap.get(leftCh) == 0){
-                        matched--;
+                char lch = s.charAt(left);
+                if(freq.containsKey(lch)){
+                    if(freq.get(lch) >= 0){
+                        remaining++;
                     }
-                    tmap.put(leftCh, tmap.getOrDefault(leftCh, 0) + 1);
+                    freq.put(lch, freq.get(lch) + 1);
                 }
+
+                left++;
             }
+
             right++;
         }
 
-        return s.substring(start, end);
+        return result == Integer.MAX_VALUE ? "" : s.substring(bestStart, bestEnd + 1);
     }
 }
