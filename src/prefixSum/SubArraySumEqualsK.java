d57f1d0077bc880525ff14844abd55f6f83490fd
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-02-21 10:35:49 +0530

    Update SubArraySumEqualsK.java

--- src/prefixSum/SubArraySumEqualsK.java
+++ src/prefixSum/SubArraySumEqualsK.java
@@ -26,15 +26,17 @@ public class SubArraySumEqualsK {
 
     private static int subarraySum(int[] nums, int k)
     {
-        Map<Integer, Integer> prefixSum = new HashMap<>();
-        prefixSum.put(0, 1);
+        Map<Integer, Integer> freq = new HashMap<>();
+        freq.put(0, 1); //base case
         int sum = 0;
         int count = 0;
 
-        for(int i=0; i<nums.length; i++){
-            sum += nums[i];
-            count += prefixSum.getOrDefault((sum - k), 0);
-            prefixSum.put(sum, prefixSum.getOrDefault(sum, 0) + 1);
+        for(int num : nums){
+            sum += num;
+
+            count += freq.getOrDefault(sum - k, 0);
+
+            freq.put(sum, freq.getOrDefault(sum, 0) + 1);
         }
 
         return count;
