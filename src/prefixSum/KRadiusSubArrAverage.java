2173710005ba76a68dc548c33e4c56d6ce3d5f5a
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-04-03 19:08:34 +0530

    Create KRadiusSubArrAverage.java

---
+++ src/prefixSum/KRadiusSubArrAverage.java
@@ -0,0 +1,38 @@
+package prefixSum;
+
+public class KRadiusSubArrAverage {
+    //https://leetcode.com/problems/k-radius-subarray-averages/description/
+
+    //todo
+
+    public int[] getAverages(int[] nums, int k)
+    {
+        int n = nums.length;
+
+        int[] radius = new int[n];
+        for(int i=0; i<n; i++){
+            radius[i] = -1; //default
+        }
+
+        int window = 2 * k + 1;
+        if (window > n) {
+            return radius;
+        }
+
+        long[] prefix = new long[n];
+        prefix[0] = nums[0];
+        for(int i=1; i<n; i++){
+            prefix[i] = prefix[i-1] + nums[i];
+        }
+
+        for(int i=k; i<n-k; i++){
+            int left = i - k;
+            int right = i + k;
+
+            long sum = prefix[right] - (left > 0 ? prefix[left - 1] : 0);
+            radius[i] = (int) (sum / window);
+        }
+
+        return radius;
+    }
+}
