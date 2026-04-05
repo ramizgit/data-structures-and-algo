ab485497e74dc069752c96e0856b79fed7effb4a
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-04-05 10:26:38 +0530

    Update LongestIncreasingSubsequence.java

--- src/dp/LongestIncreasingSubsequence.java
+++ src/dp/LongestIncreasingSubsequence.java
@@ -1,32 +1,123 @@
 package dp;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class LongestIncreasingSubSeq {
-    //https://leetcode.com/problems/longest-increasing-subsequence/description/
 
     public static void main(String[] args)
     {
-        System.out.println(longestISS(new int[]{10,9,2,5,3,7,101,18}));
-        System.out.println(longestISS(new int[]{7,7,7,7,7}));
+        //nums = [0, 8, 4, 12, 2]
+
+        //---------print just length of LIS
+        System.out.println(longestIncreasingSubSeqViaDP(new int[]{0, 8, 4, 12, 2})); //3
+        System.out.println(longestIncreasingSubSeqViaBinarySearch(new int[]{0, 8, 4, 12, 2})); //3
+
+        //----------print actual LIS
+        System.out.println(printLISViaDP(new int[]{0, 8, 4, 12, 2})); //[0, 8, 12]
+
+        //---------min deletion to sort array
+        System.out.println(minDeletionsToSortArr(new int[]{0, 8, 4, 12, 2}));
     }
 
-    private static int longestISS(int[] num)
+    //Time : O(n^2)
+    //Space : O(n)
+    private static int longestIncreasingSubSeqViaDP(int[] nums)
     {
-        int[] dp = new int[num.length];
-        Arrays.fill(dp, 1);
-        int max = 1;
+        int n = nums.length;
+        int[] dp = new int[n]; //dp array
+        Arrays.fill(dp, 1); //base case
 
-        //traverse the array backward (from end) and populate dp
-        for(int i=num.length-1; i>=0; i--){
-            for(int j=i+1; j<num.length; j++){
-                if(num[i] < num[j]){
+        int maxLen = 1; //base case, as min 1 length seq is possible
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(nums[j] < nums[i]){
                     dp[i] = Math.max(dp[i], 1 + dp[j]);
-                    max = Math.max(max, dp[i]);
                 }
+            }
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+
+        return maxLen;
+    }
+
+    //Time : O(nlogn)
+    //Space : O(n)
+    private static int longestIncreasingSubSeqViaBinarySearch(int[] nums)
+    {
+        List<Integer> list = new ArrayList<>();
+        list.add(nums[0]); //base case
+
+        for(int i=1; i<nums.length; i++){
+            //do binary search on list
+            int low = 0;
+            int high = list.size()-1;
+
+            while(low <= high){
+                int mid = low + (high - low) / 2;
+                if(nums[i] > list.get(mid)){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }
+
+            if(low == list.size()){
+                list.add(nums[i]);
+            }else{
+                list.set(low, nums[i]);
             }
         }
 
-        return max;
+        return list.size();
     }
+
+    //Time : O(n^2)
+    //Space : O(n)
+    private static List<Integer> printLISViaDP(int[] nums)
+    {
+        int n = nums.length;
+        int[] dp = new int[n]; //dp array to track max len
+        int[] prev = new int[n]; //prev array to keep track of chain
+
+        Arrays.fill(dp, 1); //base case, as each element itself is LIS
+        Arrays.fill(prev, -1);
+
+        int maxLen = 1; //base case, as min 1 length seq is possible
+        int maxIdx = 0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(nums[j] < nums[i]){
+                    if(dp[i] < 1 + dp[j]){
+                        dp[i] = 1 + dp[j];
+                        prev[i] = j;
+                    }
+                }
+            }
+
+            if(maxLen < dp[i]){
+                maxLen = dp[i];
+                maxIdx = i;
+            }
+        }
+
+        // reconstruct LIS
+        List<Integer> list = new ArrayList<>();
+
+        int curr = maxIdx;
+        while (curr != -1) {
+            list.add(nums[curr]);
+            curr = prev[curr];
+        }
+
+        Collections.reverse(list);
+        return list;
+    }
+
+    private static int minDeletionsToSortArr(int[] nums)
+    {
+        return nums.length - longestIncreasingSubSeqViaBinarySearch(nums);
+    }
+
 }
