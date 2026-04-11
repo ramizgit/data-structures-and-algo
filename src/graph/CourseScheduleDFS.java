385ad12693e5fdeb08e6a7448a6ad2c5d981b0f9
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-04-11 23:17:40 +0530

    Update CourseScheduleDFS.java

--- src/graph/CourseScheduleDFS.java
+++ src/graph/CourseScheduleDFS.java
@@ -4,70 +4,56 @@ import java.util.*;
 
 public class CourseSchedule {
 
-    public static void main(String[] args)
-    {
-        int[][] input = { {1, 0} };
-        System.out.println(canFinish(2, input)); //true
+    /*
+    Time Complexity = O(V + E)
+    V = number of courses (numCourses)
+    E = number of prerequisites
 
-        int[][] input2 = { {1, 0}, {0, 1} };
-        System.out.println(canFinish(2, input2)); //false
-
-        int[][] input3 = { {1, 0}, {2,1}, {1,2} };
-        System.out.println(canFinish(3, input3)); //false
-
-        int[][] input4 = { {1, 0}, {2,1} };
-        System.out.println(canFinish(5, input4)); //true
-
-        int[][] input5 = { {1,0}, {2,0}, {1,2}  };
-        System.out.println(canFinish(5, input5)); //true
-    }
-
-    private static boolean canFinish(int numCourses, int[][] prerequisites)
-    {
-        Map<Integer, List<Integer>> graph = buildGraph(prerequisites);
-
-        for(int i=0; i<numCourses; i++){
-            Set<Integer> visited = new HashSet<>();
-            if(cycleExists(i,graph, visited)){
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    //dfs
-    private static boolean cycleExists(int start, Map<Integer, List<Integer>> graph, Set<Integer> visited)
-    {
-        if(visited.contains(start)){
-            return true; //cycle found
-        }
-
-        visited.add(start);
-
-        //explore neighbours
-        List<Integer> neighbours = graph.getOrDefault(start, new ArrayList<>());
-
-        for(int neighbour : neighbours){
-            if(cycleExists(neighbour, graph, visited)){
-                return true;
-            }
-        }
-
-        visited.remove(start); //backtracking
-
-        return false;
-    }
-
-    private static Map<Integer, List<Integer>> buildGraph(int[][] prerequisites)
-    {
+    Space Complexity = O(V + E)
+     */
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        //initialize graph and indegree map
         Map<Integer, List<Integer>> graph = new HashMap<>();
+        Map<Integer, Integer> indegree = new HashMap<>();
 
-        for(int[] prerequisite : prerequisites){
-            graph.computeIfAbsent(prerequisite[0], k -> new ArrayList<>()).add(prerequisite[1]);
+        for(int i=0; i<numCourses; i++){ //Time : O(V)
+            graph.put(i, new ArrayList<>());
+            indegree.put(i, 0);
         }
 
-        return graph;
-    }
+        //populate graph and indegree as per prerequisites
+        for(int[] p : prerequisites){ //Time : O(E)
+            int src = p[1];
+            int des = p[0];
 
+            graph.get(src).add(des);
+            indegree.put(des, indegree.get(des) + 1);
+        }
+
+        Queue<Integer> queue = new ArrayDeque<>(); //for bfs logic
+        int processed = 0;
+
+        //collect all starting vertices with 0 indegree in the bfs queue
+        for(Map.Entry<Integer, Integer> entry : indegree.entrySet()){ //Time : O(V)
+            if(entry.getValue() == 0){
+                queue.offer(entry.getKey());
+            }
+        }
+
+        //bfs logic
+        while(!queue.isEmpty()){ //Time : O(V+E)
+            int node = queue.poll();
+            processed++;
+
+            for(int neighbour : graph.get(node)){
+                indegree.put(neighbour, indegree.get(neighbour) - 1);
+                if(indegree.get(neighbour) == 0){
+                    queue.add(neighbour); //add to bfs queue once indegree becomes 0
+                }
+            }
+        }
+
+        // if all nodes processed → no cycle
+        return processed == numCourses;
+    }
 }
