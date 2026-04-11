0d332e3eeb51a1b799b2349b8534611970494d8a
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-04-11 06:21:22 +0530

    Update LargestSquareMatrix.java

--- src/matrix/LargestSquareMatrix.java
+++ src/matrix/LargestSquareMatrix.java
@@ -1,54 +1,38 @@
-package dp;
+package matrix;
 
 public class LargestSquareMatrix {
+
     //https://leetcode.com/problems/maximal-square/description/
-    public static void main(String[] args)
-    {
-        int[][] m1 = new int[][]{ {1, 0, 1, 1, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 0, 0, 1, 0}};
-        System.out.println(getLargestSquareMatrix(m1));//3
 
-        int[][] m2 = new int[][]{ {1, 0, 1, 0, 0}, {1, 0, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 0, 0, 1, 0}};
-        System.out.println(getLargestSquareMatrix(m2));//2
+    /*
+    Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+     */
 
-        int[][] m3 = new int[][]{ {0, 1}, {1, 0} };
-        System.out.println(getLargestSquareMatrix(m3));//1
-    }
-
-    public static int getLargestSquareMatrix(int[][] matrix)
+    public int maximalSquare(int[][] matrix)
     {
         int m = matrix.length;
         int n = matrix[0].length;
 
+        int maxSquare = 0;
+
+        //dp matrix
         int[][] dp = new int[m][n];
-        int max = 0;
 
-        //populate first column of dp
-        for(int i=0; i<m; i++)
-        {
-            dp[i][0] = matrix[i][0];
-            max = Math.max(max, dp[i][0]);
-        }
+        //populate rest of dp matrix
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == 1){
+                    if(i == 0 || j == 0){
+                        dp[i][j] = 1; //base case
+                    }else{
+                        dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i][j-1], dp[i-1][j]));
+                    }
 
-        //populate first row of dp
-        for(int j=0; j<n; j++)
-        {
-            dp[0][j] = matrix[0][j];
-            max = Math.max(max, dp[0][j]);
-        }
-
-        //populate rest of the dp matrix using previously calculated values
-        for(int i=1; i<m; i++)
-        {
-            for(int j=1; j<n ;j++)
-            {
-                if(matrix[i][j] == 0){
-                    dp[i][j] = 0;
-                }else if(matrix[i][j] == 1){
-                    dp[i][j] = Math.min(Math.min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1]) + 1;
-                    max = Math.max(max, dp[i][j]);
+                    maxSquare = Math.max(maxSquare, dp[i][j]);
                 }
             }
         }
-        return max;
+
+        return maxSquare * maxSquare; //return area
     }
 }
