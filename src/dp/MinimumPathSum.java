57bf4c6346fad4409926c9454622733f02ced4f3
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-04-11 17:48:15 +0530

    Update MinimumPathSum.java

--- src/dp/MinimumPathSum.java
+++ src/dp/MinimumPathSum.java
@@ -30,7 +30,7 @@ public class MinimumPathSum {
         //populate rest
         for(int i=1; i<m; i++){
             for(int j=1; j<n; j++){
-                dp[i][j] = Math.min(grid[i][j] + dp[i-1][j], grid[i][j] + dp[i][j-1]);
+                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
             }
         }
 
