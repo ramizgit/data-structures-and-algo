664068a98e08c1349548ec1c1be719421ca03cc5
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-04-23 15:01:59 +0530

    Update InsertInterval.java

--- src/intervals/InsertInterval.java
+++ src/intervals/InsertInterval.java
@@ -1,43 +1,38 @@
 package intervals;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class InsertInterval {
-    //https://leetcode.com/problems/insert-interval/description/
-    public static void main(String[] args)
-    {
-        insertInterval(new int[][]{{1,3}, {6,9}}, new int[]{2,5});
-        insertInterval(new int[][]{{1,2}, {3,5}, {6,7}, {8,10},{12,16}}, new int[]{4,8});
-    }
+public class InsertIntervals {
 
-    private static int[][] insertInterval(int[][] intervals, int[] newInterval)
+    //https://leetcode.com/problems/insert-interval/
+
+    public int[][] insert(int[][] intervals, int[] newInterval)
     {
         List<int[]> result = new ArrayList<>();
+
         int idx = 0;
 
-        //step1: add all intervals coming before new interval
-        while (idx < intervals.length && intervals[idx][1] < newInterval[0]){
+        //step1 : add intervals ending before the new interval start time
+        while(idx < intervals.length && intervals[idx][1] < newInterval[0]){
             result.add(intervals[idx]);
             idx++;
         }
 
-        //step2: merge intervals if any
-        while (idx < intervals.length && intervals[idx][0] <= newInterval[1]){
-            newInterval[0] = Math.min(intervals[idx][0], newInterval[0]);
-            newInterval[1] = Math.max(intervals[idx][1], newInterval[1]);
+        //step2 : merge overlapping intervals if any
+        //stretch the new interval to absorb all overlaps, then insert it once.
+        while(idx < intervals.length && intervals[idx][0] <= newInterval[1]){
+            newInterval[0] = Math.min(newInterval[0], intervals[idx][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[idx][1]);
             idx++;
         }
         result.add(newInterval);
 
-        //step3: add all intervals coming after new interval
-        while (idx < intervals.length){
+        //step3 : add rest of the intervals coming after the new interval
+        while(idx < intervals.length){
             result.add(intervals[idx]);
             idx++;
         }
 
-        //return final result
         return result.toArray(new int[result.size()][]);
     }
 }
-
