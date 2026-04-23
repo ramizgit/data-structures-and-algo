c5d01456e7a90012066b2486fa2345c5149573dc
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-04-23 09:38:10 +0530

    Update MeetingRoom.java

--- src/intervals/MeetingRoom.java
+++ src/intervals/MeetingRoom.java
@@ -1,33 +1,31 @@
 package intervals;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class MeetingRoom {
+
     //https://leetcode.com/problems/meeting-rooms/description/
-    public static void main(String[] args)
-    {
-        System.out.println(canAttendMeetings(new int[][]{{0, 30}, {5, 10}, {15, 20}})); //false
-        System.out.println(canAttendMeetings(new int[][]{{5, 8}, {9, 15}})); //true
-    }
 
     private static boolean canAttendMeetings(int[][] intervals)
     {
-        List<int[]> input = new ArrayList<>();
-        for(int[] interval : intervals){
-            input.add(interval);
+        //input validation
+        if (intervals == null || intervals.length == 0) {
+            return true;
         }
-        Collections.sort(input, (a, b) -> a[0] - b[0]);
 
-        for(int i=1; i<input.size(); i++){
-            int[] prevInterval = input.get(i-1);
-            int[] currInterval = input.get(i);
-            if(currInterval[0] < prevInterval[1]){
-                //overlap
-                return false;
+        //sort input intervals by start time in asc order
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        //check adjacent intervals for any overlap
+        for(int i=1; i<intervals.length; i++){
+            int[] curr = intervals[i];
+            int[] prev = intervals[i-1];
+
+            if(curr[0] < prev[1]){
+                return false; //overlap found
             }
         }
-        return true;
+
+        return true; //no overlap found
     }
 }
