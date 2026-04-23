6c160de627b290e2f4edb84d73c5d5406ad94754
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-04-23 17:25:10 +0530

    Update RemoveCoveredIntervals.java

--- src/intervals/RemoveCoveredIntervals.java
+++ src/intervals/RemoveCoveredIntervals.java
@@ -6,6 +6,13 @@ public class RemoveCoveredIntervals {
 
     //https://leetcode.com/problems/remove-covered-intervals/description/
 
+    /*
+    Given an array intervals where intervals[i] = [li, ri] represent the interval [li, ri),
+    remove all intervals that are covered by another interval in the list.
+    The interval [a, b) is covered by the interval [c, d) if and only if c <= a and b <= d.
+    Return the number of remaining intervals.
+     */
+
     public int removeCoveredIntervals(int[][] intervals)
     {
         if(intervals == null || intervals.length == 0){
@@ -16,7 +23,9 @@ public class RemoveCoveredIntervals {
 
         //sort input intervals so that bigger intervals come first and smaller (covered) ones come later
         Arrays.sort(intervals, (a, b) -> {
-            if (a[0] != b[0]) return a[0] - b[0];
+            if (a[0] != b[0]) {
+                return a[0] - b[0];
+            }
             return b[1] - a[1];
         });
 
