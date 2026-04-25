40ace5df2cc911e52c10957d7429d4f441e68b93
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-04-25 12:14:03 +0530

    Update DailyTemperatures.java

--- src/stack/DailyTemperatures.java
+++ src/stack/DailyTemperatures.java
@@ -3,33 +3,31 @@ package stack;
 import java.util.Stack;
 
 public class DailyTemperatures {
+
     //https://leetcode.com/problems/daily-temperatures/description/
-    public static void main(String[] args)
-    {
-        dailyTemperatures(new int[]{73,74,75,71,69,72,76,73});//[1,1,4,2,1,1,0,0]
-        dailyTemperatures(new int[]{30,40,50,60});//[1,1,1,0]
-        dailyTemperatures(new int[]{30,60,90});//[1,1,0]
-    }
 
     private static int[] dailyTemperatures(int[] temperatures)
     {
-        Stack<Integer> stack = new Stack<>();
-        int[] output = new int[temperatures.length];
+        //input validation
+        if(temperatures == null || temperatures.length == 0){
+            return new int[0];
+        }
 
-        for(int i=temperatures.length-1; i>=0; i--){
+        int n = temperatures.length;
+        Stack<Integer> stack = new Stack<>();
+        int[] answer = new int[n];
+
+        for(int i=n-1; i>=0; i--){
+            //keep on popping from stack till warmer temp found
             while (!stack.empty() && temperatures[i] > temperatures[stack.peek()]){
                 stack.pop();
             }
 
-            if(stack.empty()){
-                output[i] = 0;
-            }else {
-                output[i] = stack.peek() - i;
-            }
-            stack.push(i);
+            answer[i] = stack.isEmpty() ? 0 : stack.peek() - i; //num of days diff
+
+            stack.push(i); //push current temp to stack
         }
 
-        return output;
+        return answer;
     }
 }
-
