7436e932cd97dd707659d67db1b6e9b97be52bd4
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-04-27 06:45:33 +0530

    Create MinimumAreaRectangle.java

---
+++ src/google/MinimumAreaRectangle.java
@@ -0,0 +1,52 @@
+package google;
+
+import java.util.*;
+
+public class MinimumAreaRectangle {
+
+    //https://leetcode.com/problems/minimum-area-rectangle/description/
+
+    /*
+    You are given an array of points in the X-Y plane points where points[i] = [xi, yi].
+    Return the minimum area of a rectangle formed from these points, with sides parallel to the X and Y axes.
+    If there is not any such rectangle, return 0.
+     */
+
+    public int minAreaRect(int[][] points)
+    {
+        //store points in hashset for quick O(1) lookup
+        Set<String> pointsSet = new HashSet<>();
+        for(int[] point : points){
+            pointsSet.add(point[0]+","+point[1]);
+        }
+
+        int minArea = Integer.MAX_VALUE;
+
+        //iterate all pairs, check if rectangle feasbile, calculate area
+        for(int i=0; i<points.length; i++){
+            for(int j=i+1; j<points.length; j++){
+                int[] p1 = points[i];
+                int[] p2 = points[j];
+
+                int x1 = p1[0];
+                int y1 = p1[1];
+                int x2 = p2[0];
+                int y2 = p2[1];
+
+                if (x1 == x2 || y1 == y2) {
+                    continue; //ignore pairs that are not diagonals
+                }
+
+                //check diagonals
+                String diagonal1 = x1+","+y2; //{x1, y2};
+                String diagonal2 = x2+","+y1;// {x2, y1};
+                if(pointsSet.contains(diagonal1) && pointsSet.contains(diagonal2)){
+                    int area = Math.abs(x1 - x2) * Math.abs(y1 - y2);
+                    minArea = Math.min(minArea, area);
+                }
+            }
+        }
+
+        return minArea == Integer.MAX_VALUE ? 0 : minArea;
+    }
+}
