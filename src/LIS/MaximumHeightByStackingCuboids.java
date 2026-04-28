87aa84fc4b4988d3da9ed11a4e527bf3ef82c014
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-04-28 05:45:31 +0530

    Update MaximumHeightByStackingCuboids.java

--- src/LIS/MaximumHeightByStackingCuboids.java
+++ src/LIS/MaximumHeightByStackingCuboids.java
@@ -2,17 +2,28 @@ package longestIncreasingSubseqVariants;
 
 import java.util.*;
 
+
+/*
+“Sorting each cuboid removes rotation complexity by fixing a canonical orientation. Sorting all cuboids ensures a valid order for LIS-style DP,
+where we build stacks from smaller to larger cuboids.”
+ */
+
 public class MaximumHeightByStackingCuboids {
 
     //todo : practice
 
     public int maxHeight(int[][] cuboids) {
-        // Step 1: sort each cuboid
+        // Step 1: sort each cuboid for normalization
+        /*
+        If a cuboid can be rotated to fit somewhere, then its sorted version will also fit in that same position.
+        Always use the smallest sides as base, largest as height. If stacking is possible in any rotation, it will also be possible in this normalized form.
+         */
         for (int[] c : cuboids) {
             Arrays.sort(c);
         }
 
         // Step 2: sort all cuboids
+        //this ensures when we process a box, all possible boxes that can go above it are already processed, primarily for DP concept
         Arrays.sort(cuboids, (a, b) -> {
             if (a[0] != b[0]) return a[0] - b[0]; //width
             if (a[1] != b[1]) return a[1] - b[1]; //length
