1f2f5c96d26e046ed2f9342244782e16236cafef
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-05-01 08:46:16 +0530

    Update MinimumNumOfPlatform.java

--- src/google/MinimumNumOfPlatform.java
+++ src/google/MinimumNumOfPlatform.java
@@ -55,4 +55,54 @@ public class MinimumNumOfPlatform {
 
         return maxTrains; //minimum number of platforms we need
     }
+
+    public int[] assignPlatformNumbers(int[] arr, int[] dep)
+    {
+        int n = arr.length;
+
+        //create list of trains from input
+        List<Train> trains = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            trains.add(new Train(arr[i], dep[i], i));
+        }
+
+        trains.sort((a, b) -> a.arr - b.arr); //sort by arrival time
+
+        //minheap for departure times, to get time of earliest platform availability
+        PriorityQueue<Train> minheap = new PriorityQueue<>( (a, b) -> a.dep - b.dep );
+
+        int[] result = new int[n];
+        int platform = 0;
+
+        for(Train train : trains){
+            if(!minheap.isEmpty() && train.arr > minheap.peek().dep){
+                //reuse
+                Train earliestDep = minheap.poll();
+                train.platform = earliestDep.platform;
+                result[train.idx] = train.platform;
+
+            }else{
+                //assign new platform
+                platform++;
+                train.platform = platform;
+                result[train.idx] = train.platform;
+            }
+            minheap.add(train);
+        }
+
+        return result;
+    }
+
+    class Train{
+        int arr;
+        int dep;
+        int platform;
+        int idx;
+
+        public Train(int arr, int dep, int idx) {
+            this.arr = arr;
+            this.dep = dep;
+            this.idx = idx;
+        }
+    }
 }
