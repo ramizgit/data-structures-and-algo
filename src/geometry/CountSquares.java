dcde7c6c02be89eb8570506441ba87d121b83402
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-05-03 19:43:08 +0530

    Update CountSquares.java

--- src/geometry/CountSquares.java
+++ src/geometry/CountSquares.java
@@ -44,4 +44,61 @@ public class CountSquares {
 
         return sqCount;
     }
+
+    /*
+    squares are not axis aligned, they are rotated at any angle on the plane
+     */
+    
+    /*
+    APPROACH:-
+     Pick any 2 points as a potential side AB.
+     Compute vector (dx, dy). A square’s adjacent side is a perpendicular vector of same length.
+     Rotate (dx, dy) by ±90° → (-dy, dx) and (dy, -dx) to get the only two possible squares on either side.
+     Add this rotated vector to both A and B to get the other two points (C and D).
+     If both points exist in the set → valid square.
+     Each square is counted 4 times (once per side), so divide final count by 4.
+     */
+    public static int countSquares2(int[][] points) {
+        int n = points.length;
+
+        Set<String> set = new HashSet<>();
+        for (int[] p : points) {
+            set.add(p[0] + "," + p[1]);
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                int x1 = points[i][0], y1 = points[i][1];
+                int x2 = points[j][0], y2 = points[j][1];
+
+                int dx = x2 - x1;
+                int dy = y2 - y1;
+
+                // rotation 1
+                int x3 = x1 - dy, y3 = y1 + dx;
+                int x4 = x2 - dy, y4 = y2 + dx;
+
+                if (set.contains(x3 + "," + y3) &&
+                        set.contains(x4 + "," + y4)) {
+                    count++;
+                }
+
+                // rotation 2
+                x3 = x1 + dy; y3 = y1 - dx;
+                x4 = x2 + dy; y4 = y2 - dx;
+
+                if (set.contains(x3 + "," + y3) &&
+                        set.contains(x4 + "," + y4)) {
+                    count++;
+                }
+            }
+        }
+
+        // each square counted 4 times (each side)
+        return count / 4;
+    }
+
 }
