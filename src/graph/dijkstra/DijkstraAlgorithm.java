70c16858f4df3d9fcd54c3d62b5a1ac4924e2f5a
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-05-05 08:29:38 +0530

    Update DijkstraAlgorithm.java

--- src/graph/dijkstra/DijkstraAlgorithm.java
+++ src/graph/dijkstra/DijkstraAlgorithm.java
@@ -5,32 +5,38 @@ import java.util.*;
 public class DijkstraAlgorithm {
 
     //undirected weighted graph
-    //O((V+E)logV)
-    private static int[] dijkstra(int v, int[][] edges, int source) {
+    //O((V+E)logE)
+    private int[] dijkstra(int n, int[][] edges, int source) {
 
         //initialize graph
         Map<Integer, List<Edge>> graph = new HashMap<>();
-        for(int i=0; i<v; i++){
+        for(int i=0; i<n; i++){ //O(V)
             graph.put(i, new ArrayList<>());
         }
 
         //populate edges
-        for(int[] edge : edges){
-            graph.get(edge[0]).add(new Edge(edge[1], edge[2]));
-            graph.get(edge[1]).add(new Edge(edge[0], edge[2])); //undirected graph, hence adding both
+        for(int[] edge : edges){ //O(E)
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            
+            graph.get(u).add(new Edge(v, w));
+            graph.get(v).add(new Edge(u, w)); //undirected graph, hence adding both
         }
 
-        int[] dist = new int[v];
-        Arrays.fill(dist, Integer.MAX_VALUE); //initial values, to be relaxed later
+        //dijkstra algo
+        PriorityQueue<Edge> pq = new PriorityQueue<>( (a,b) -> a.w - b.w ); //minheap
+        pq.offer(new Edge(source, 0)); //starting point
 
-        PriorityQueue<Edge> pq = new PriorityQueue<>( (a,b) -> a.w - b.w );
-        pq.offer(new Edge(source, 0));
+        int[] dist = new int[n]; //distance/cost array, our goal to minimize it
+        Arrays.fill(dist, Integer.MAX_VALUE); //initial values, to be relaxed later
         dist[source] = 0; //starting point
 
         while(!pq.isEmpty()){
-            Edge curr = pq.poll();
+            Edge curr = pq.poll(); //O(logE)
 
-            if(curr.w > dist[curr.v]){ //stale entry
+            //stale entry check
+            if(curr.w > dist[curr.v]){
                 /*
                 If current weight is more than what we already computed, then ignore, as it is outdated.
                 When you update a distance (relax an edge), you push a new entry into the priority queue:
@@ -43,27 +49,29 @@ public class DijkstraAlgorithm {
                 continue;
             }
 
+            //explore neighbours
             List<Edge> neighbours = graph.get(curr.v);
             for(Edge neighbour : neighbours){
-                int currDist = dist[neighbour.v];
                 int newDist = curr.w + neighbour.w;
+                int currDist = dist[neighbour.v];
                 if(newDist < currDist){
                     dist[neighbour.v] = newDist; //relaxation
-                    pq.offer(new Edge(neighbour.v, newDist));
+                    pq.offer(new Edge(neighbour.v, newDist)); //O(logE)
                 }
             }
         }
 
         return dist;
     }
-}
 
-class Edge{
-    int v;
-    int w;
+    class Edge{
+        int v;
+        int w;
 
-    public Edge(int v, int w) {
-        this.v = v;
-        this.w = w;
+        public Edge(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
     }
 }
+
