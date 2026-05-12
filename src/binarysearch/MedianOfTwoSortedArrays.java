ab72b2fe4463af955eb3c64db02054233db6c0e4
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-05-12 07:09:24 +0530

    Update MedianOfTwoSortedArrays.java

--- src/binarysearch/MedianOfTwoSortedArrays.java
+++ src/binarysearch/MedianOfTwoSortedArrays.java
@@ -1,50 +1,56 @@
-package binarysearch;
+package binarySearch;
+
+/*
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+The overall run time complexity should be O(log (m+n)).
+ */
 
 public class MedianOfTwoSortedArrays {
-    public static void main(String[] args)
-    {
-        System.out.println(findMedianSortedArrays(new int[]{1,2}, new int[]{3,4})); //2.5
-        System.out.println(findMedianSortedArrays(new int[]{1,2}, new int[]{3,4,5})); //2.5
-    }
 
-    private static double findMedianSortedArrays(int[] nums1, int[] nums2)
+    //https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2)
     {
-        //make sure nums1 is smaller to run binary search on it
+        //make sure first arr is smaller, to run binary search on it
         if(nums1.length > nums2.length){
             return findMedianSortedArrays(nums2, nums1);
         }
 
-        int l1 = nums1.length;
-        int l2 = nums2.length;
-        int totalLen = l1 + l2;
-        int haflLen = totalLen / 2;
+        int m = nums1.length;
+        int n = nums2.length;
+        int totalLen = m + n;
+        int halfLen = (totalLen + 1) / 2;
 
-        //run BS on nums1
-        int left = 0;
-        int right = l1-1;
+        //run binary search on nums1 length (not index)
+        int low = 0;
+        int high = m; //binary search on length, not index (important)
 
-        while(left <= right){
-            int mid1 = (left + right) / 2;
-            int mid2 = haflLen - mid1 - 2;
+        while(low <= high){
+            int mid1 = low + (high - low) / 2;
+            int mid2 = halfLen - mid1;
 
-            int left1 = mid1 >=0 ? nums1[mid1] : Integer.MIN_VALUE;
-            int right1 = mid1 < nums1.length-1 ? nums1[mid1+1] : Integer.MAX_VALUE;
-            int left2 = mid2 >=0 ?nums2[mid2] : Integer.MIN_VALUE;
-            int right2 = mid2 < nums2.length-1 ? nums2[mid2+1] : Integer.MAX_VALUE;
+            //note : mid1 is NOT an index, it represents how many elements are taken into left partition
+            int left1 = (mid1 == 0) ? Integer.MIN_VALUE : nums1[mid1 - 1];
+            int right1 = (mid1 == m) ? Integer.MAX_VALUE : nums1[mid1];
+            int left2 = (mid2 == 0) ? Integer.MIN_VALUE : nums2[mid2 - 1];
+            int right2 = (mid2 == n) ? Integer.MAX_VALUE : nums2[mid2];
 
-            if(left1 <= right2 && left2 <= right1){
-                if(totalLen %2 == 0){
+            // correct partition
+            if(left1 <= right2 && left2 <= right1) {
+                if(totalLen % 2 == 0){
+                    // even length
                     return (Math.max(left1, left2) + Math.min(right1, right2)) / 2.0;
                 }else{
-                    return Math.min(right1, right2);
+                    // odd length
+                    return Math.max(left1, left2);
                 }
-            } else if (left1 > right2) {
-                right = mid1 - 1;
+            }else if (left1 > right2) {
+                high = mid1 - 1; //took too many elements from nums1, move left
             } else {
-                left = mid1 + 1;
+                low = mid1 + 1; //left2 > right1 -> need more elements on left side from nums1, move right
             }
         }
+
         return 0.0;
     }
 }
-
