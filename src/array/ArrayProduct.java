8bae3eda1442e59fd24067f9e50fb5864f49acde
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-05-12 14:21:32 +0530

    Update ArrayProduct.java

--- src/array/ArrayProduct.java
+++ src/array/ArrayProduct.java
@@ -1,13 +1,38 @@
-package array;
+package arrays;
 
-public class ArrayProduct {
-    
-    public static void main(String[] args)
+public class ProductOfArrayExceptSelf {
+
+    //https://leetcode.com/problems/product-of-array-except-self/description/
+
+    public int[] getProduct_constantSpace(int[] arr)
     {
-        getProduct(new int[]{1,2,3,4});
+        if(arr == null || arr.length == 0){
+            return new int[0];
+        }
+
+        int n = arr.length;
+
+        //traverse left to right, store prefix in result
+        int[] result = new int[n];
+        result[0] = 1;
+
+        for(int i=1; i<n; i++){
+            result[i] = result[i-1] * arr[i-1];
+        }
+
+        //traverse right to left and calculate suffix on the fly
+        int suffix = 1;
+        for(int i=n-1; i>=0; i--){
+            result[i] *= suffix;
+            suffix *= arr[i];
+        }
+
+        return result;
     }
 
-    public static void getProduct(int[] arr)
+
+    //O(n) space
+    public int[] getProduct(int[] arr)
     {
         int[] leftProduct = new int[arr.length];
         int[] rightProduct = new int[arr.length];
@@ -32,5 +57,7 @@ public class ArrayProduct {
         {
             finalProduct[i] = leftProduct[i] * rightProduct[i];
         }
+
+        return finalProduct;
     }
 }
