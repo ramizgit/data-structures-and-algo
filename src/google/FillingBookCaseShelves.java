7ebc26e91e900b8b4f3920eb603057554d5ad35e
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-05-13 05:40:32 +0530

    Update FillingBookCaseShelves.java

--- src/google/FillingBookCaseShelves.java
+++ src/google/FillingBookCaseShelves.java
@@ -6,48 +6,42 @@ public class FillingBookCaseShelves {
 
     //https://leetcode.com/problems/filling-bookcase-shelves/description/
 
-    //todo
-
     public int minHeightShelves(int[][] books, int shelfWidth)
     {
-        //edge case
         if(books == null || books.length == 0){
             return 0;
         }
 
-        int n = books.length;
+        int n = books.length; //number of books
 
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0; //base case, no books, no height
+        int[] dp = new int[n]; // dp[i] = minimum height to place books from 0 to i
 
-        for(int i=1; i<=n; i++){
+        for(int i=0; i<n; i++){
             //for each book try both approach
             //1. start with separate shelf
             //2. keep on combining with prev books as long as width constrant permits
 
-            int currWidth = 0;
-            int currHeight = 0;
+            //1. place current book on new shelf
+            int currWidth = books[i][0];
+            int currHeight = books[i][1];
+            dp[i] = currHeight  + (i > 0 ? dp[i - 1] : 0);
 
-            for(int j=i; j>=1; j--){
-                currWidth += books[j-1][0];
+            //2. try placing previous books on same shelf
+            for(int j=i-1; j>=0; j--){ //since books need to be places in order
+
+                currWidth += books[j][0];
 
                 if(currWidth > shelfWidth){
                     break; //cant proceed as it exceeds total shelf width constraint
                 }
 
-                currHeight = Math.max(currHeight, books[j-1][1]);
+                // books[j...i] are placed on same shelf
+                currHeight = Math.max(currHeight, books[j][1]);
 
-                dp[i] = Math.min(
-                        dp[i], // best answer found so far for first i books
-
-                        dp[j-1] + currHeight
-                        // books[0 ... j-2] arranged optimally
-                        // books[j-1 ... i-1] placed on current(last) shelf
-                );
+                dp[i] = Math.min(dp[i], currHeight + (j > 0 ? dp[j - 1] : 0));
             }
         }
 
-        return dp[n];
+        return dp[n-1];
     }
 }
