fda5ee591b85470fee62e59d464b773521a9adfd
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-05-16 08:34:34 +0530

    Update FruitIntoBaskets.java

--- src/sliding_window/FruitIntoBaskets.java
+++ src/sliding_window/FruitIntoBaskets.java
@@ -4,32 +4,35 @@ import java.util.*;
 
 public class FruitIntoBaskets {
 
-    public static void main(String[] args)
-    {
-
-    }
+    //https://leetcode.com/problems/fruit-into-baskets/description/
 
     public int totalFruit(int[] fruits)
     {
+        //pattern : Longest subarray with at most 2 distinct integers
+
+        //input validation
         if(fruits == null || fruits.length == 0){
             return 0;
         }
 
-        Map<Integer, Integer> freq = new HashMap<>();
+        Map<Integer, Integer> freq = new HashMap<>(); //map to track frequency of picked fruits
 
         int left = 0;
         int right = 0;
         int maxFruits = 0;
 
         while(right < fruits.length){
-            freq.put(fruits[right], freq.getOrDefault(fruits[right], 0) + 1);
+
+            //pick current fruit and increase its freq
+            int currentFruit = fruits[right];
+            freq.put(currentFruit, freq.getOrDefault(currentFruit, 0) + 1);
 
             while(freq.size() > 2){
-                int l = fruits[left];
-                freq.put(l, freq.get(l) - 1);
+                int leftFruit = fruits[left];
+                freq.put(leftFruit, freq.get(leftFruit) - 1);
 
-                if(freq.get(l) == 0){
-                    freq.remove(l);
+                if(freq.get(leftFruit) == 0){
+                    freq.remove(leftFruit);
                 }
 
                 left++;
