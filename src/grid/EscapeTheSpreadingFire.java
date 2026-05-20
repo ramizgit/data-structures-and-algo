47fb02b201bf71a3a6a553553ae755470f9e927d
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-05-20 12:07:16 +0530

    Create EscapeTheSpreadingFire.java

---
+++ src/grid/EscapeTheSpreadingFire.java
@@ -0,0 +1,182 @@
+package grid;
+
+import java.util.*;
+
+public class EscapeTheSpreadingFire {
+
+    //https://leetcode.com/problems/escape-the-spreading-fire/description/
+
+    public int maximumMinutes(int[][] grid)
+    {
+        //input validation
+        if(grid == null || grid.length == 0){
+            return -1;
+        }
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        //collect all fire cells for multi source bfs
+        Queue<State> fireBfsQueue = new ArrayDeque<>(); //queue for bfs
+        boolean[][] visited = new boolean[m][n]; //visited tracking during bfs
+
+        int[][] fireTime = new int[m][n]; //grid to hold fire timeline
+        for(int i=0; i<m; i++){
+            Arrays.fill(fireTime[i], Integer.MAX_VALUE);
+        }
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 1){
+                    fireBfsQueue.offer(new State(i, j, 0));
+                    visited[i][j] = true;
+                    fireTime[i][j] = 0;
+                }
+            }
+        }
+
+        int[][] directions = { {0, 1}, //right
+                {0, -1}, //left
+                {1, 0}, //down
+                {-1, 0} }; //up
+
+        while(!fireBfsQueue.isEmpty()){
+            State curr = fireBfsQueue.poll();
+
+            //explore neighbours
+            for(int[] dir : directions){
+                int x = dir[0] + curr.row;
+                int y = dir[1] + curr.col;
+
+                //boundary check
+                if(x < 0 || x>= m || y < 0 || y >= n){
+                    continue; //out of boundary
+                }
+
+                //wall check
+                if(grid[x][y] == 2){
+                    continue; //wall, cant continue
+                }
+
+                //visited check
+                if(visited[x][y]){
+                    continue; //already visited
+                }
+
+                int newFireTime = curr.time + 1;
+
+                //enqueue
+                fireBfsQueue.offer(new State(x, y, newFireTime));
+
+                //mark visited
+                visited[x][y] = true;
+
+                //populate fire timeline grid
+                fireTime[x][y] = newFireTime;
+            }
+        }
+
+        //now do binary search for feasibility check to get max wait time
+        int low = 0;
+        int high = m * n;
+        int answer = -1;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(canSafelyEscape(grid, fireTime, mid, m, n)){
+                answer = mid; //possible answer
+                low = mid + 1; //try higher to maximize wait time
+            }else{
+                high = mid - 1; //try lower as cant safely pass with current wait time
+            }
+        }
+
+        return answer == m * n ? 1_000_000_000 : answer;
+    }
+
+    private boolean canSafelyEscape(int[][] grid, int[][] fireTime, int waitTime, int m, int n)
+    {
+        if(waitTime >= fireTime[0][0]){
+            return false; //cannot even stay initially
+        }
+
+        Queue<State> playerBfsQueue = new ArrayDeque<>();
+        playerBfsQueue.offer(new State(0, 0, waitTime)); //starting cell
+
+        boolean[][] visited = new boolean[m][n];
+        visited[0][0] = true; //starting cell
+
+        int[][] directions = { {0, 1}, //right
+                {0, -1}, //left
+                {1, 0}, //down
+                {-1, 0} }; //up
+
+        while(!playerBfsQueue.isEmpty()){
+            State curr = playerBfsQueue.poll();
+
+            //early exit - safehouse reached
+            if(curr.row == m-1 && curr.col == n-1){
+                return true;
+            }
+
+            //explore neighbours
+            for(int[] dir : directions){
+                int x = dir[0] + curr.row;
+                int y = dir[1] + curr.col;
+
+                //boundary check
+                if(x < 0 || x>= m || y < 0 || y >= n){
+                    continue; //out of boundary
+                }
+
+                //wall check
+                if(grid[x][y] == 2){
+                    continue; //wall, cant continue
+                }
+
+                //visited check
+                if(visited[x][y]){
+                    continue; //already visited
+                }
+
+                int newTime = curr.time + 1;
+
+                boolean isSafeHouse = (x == m - 1 && y == n - 1);
+
+                //SPECIAL SAFEHOUSE RULE
+                if(isSafeHouse){
+                    //can arrive same time as fire
+                    if(newTime > fireTime[x][y]){
+                        continue;
+                    }
+                }else{
+                    //must arrive strictly before fire
+                    if(newTime >= fireTime[x][y]){
+                        continue;
+                    }
+                }
+
+                //enqueue
+                playerBfsQueue.offer(new State(x, y, newTime));
+
+                //mark visited
+                visited[x][y] = true;
+            }
+        }
+
+        return false;
+    }
+
+    class State{
+        int row;
+        int col;
+        int time;
+
+        public State(int row, int col, int time) {
+            this.row = row;
+            this.col = col;
+            this.time = time;
+        }
+    }
+}
