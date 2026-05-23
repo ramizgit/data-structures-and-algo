091c63ab6120a80859cdde5c3aa7574adaeb9021
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-05-23 19:28:50 +0530

    Update LeastFrequentlyUsedCacheImpl.java

--- src/misc/LeastFrequentlyUsedCacheImpl.java
+++ src/misc/LeastFrequentlyUsedCacheImpl.java
@@ -1,116 +1,115 @@
 package misc;
 
-import java.util.*;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 public class LeastFrequentlyUsedCacheImpl {
 
-    public static void main(String[] args)
-    {
-        LFUInterface lfu = new LFUInterfaceImpl();
+    interface LFUCache {
+        void put(String key, String val);
+        String get(String key);
+        int size();
+        boolean isEmpty();
+    }
 
-        lfu.put("k1", "v1");
-        lfu.put("k2", "v2");
-        lfu.put("k3", "v3");
+    class LFUCacheImpl implements LFUCache {
 
-        lfu.state();
-        System.out.println(lfu.get("k1"));
-        lfu.state();
-        System.out.println(lfu.get("k1"));
-        lfu.state();
-        System.out.println(lfu.get("k2"));
-        lfu.state();
+        private Map<String, Node> keyValueMap = new HashMap<>();
+        private Map<Integer, Deque<String>> frequencyKeyListMap = new HashMap<>(); //frequency → ordered keys (for LRU within same frequency)
 
-        System.out.println(lfu.get("k3"));
-        lfu.state();
+        private static final int CAPACITY = 3;
+        private int leastFreq = 0;
 
-        lfu.put("k4", "v4");
-        lfu.state();
+        @Override
+        public void put(String key, String val) {
 
-        System.out.println(lfu.get("k9"));
-        lfu.state();
+            //edge case
+            if (CAPACITY == 0) {
+                return;
+            }
+
+            //scenario if key already exists in the map
+            if(keyValueMap.containsKey(key)){
+                Node existing = keyValueMap.get(key);
+                existing.value = val; //update existing value
+
+                get(key); // treat update as access
+                return;
+            }
+
+            //scenario where its a brand new key
+            if(keyValueMap.size() == CAPACITY){
+                //remove LFU, if tie, LRU
+                Deque<String> deque = frequencyKeyListMap.get(leastFreq); //LFU
+                String lastKey = deque.removeLast(); //LRU
+                if (deque.isEmpty()) {
+                    frequencyKeyListMap.remove(leastFreq);
+                }
+                keyValueMap.remove(lastKey);
+            }
+
+            //populate map with new key val
+            keyValueMap.put(key, new Node(val));
+            frequencyKeyListMap.computeIfAbsent(1, k -> new LinkedList<>()).addFirst(key);
+
+            //set least freq to 1
+            leastFreq = 1;
+        }
+
+        @Override
+        public String get(String key) {
+
+            Node curr = keyValueMap.get(key);
+
+            if (curr == null) {
+                return null; //key not present in the cache
+            }
+
+            int oldFreq = curr.freq;
+            int newFreq = oldFreq + 1;
+
+            //remove from old entry and update least frequency
+            Deque<String> currDeque = frequencyKeyListMap.get(oldFreq);
+            currDeque.remove(key);
+
+            if (currDeque.isEmpty()) {
+                frequencyKeyListMap.remove(oldFreq);
+
+                if (oldFreq == leastFreq) {
+                    leastFreq++;
+                }
+            }
+
+            curr.freq = newFreq; //update freq in key -> val map
+            frequencyKeyListMap.computeIfAbsent(newFreq, k -> new LinkedList<>()).addFirst(key); //promote to new freq list
+
+            return curr.value;
+        }
+
+        @Override
+        public int size() {
+            return keyValueMap.size();
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return keyValueMap.isEmpty();
+        }
+    }
+
+    class Node {
+        String value;
+        int freq;
+
+        public Node(String value)
+        {
+            this.value = value;
+            this.freq = 1;
+        }
     }
 }
 
-interface LFUInterface{
-    void put(String key, String val);
-    String get(String key);
-    int size();
-    boolean isEmpty();
-    void state();
-}
 
-class LFUInterfaceImpl implements LFUInterface{
 
-    private Map<String, String> keyValmap = new HashMap<>();
-    private Map<String, Integer> keyFreqmap = new HashMap<>();
-    private Map<Integer, Deque<String>> freqListmap = new HashMap<>();
-
-    private static int LIMIT = 3;
-    private int leastFreq = Integer.MAX_VALUE;
-
-    @Override
-    public void put(String key, String val) {
-        //scenario if key already exists in the map
-        if(keyValmap.containsKey(key)){
-            keyValmap.put(key, val);
-            get(key); // treat update as access
-            return;
-        }
-
-        //scenario where its a brand new key
-        if(keyValmap.size() == LIMIT){
-            //remove LFU, if tie, LRU
-            Deque<String> deque = freqListmap.get(leastFreq); //LFU
-            String last = deque.removeLast(); //LRU
-            keyFreqmap.remove(last);
-            keyValmap.remove(last);
-        }
-
-        //populate map with new key val
-        keyValmap.put(key, val);
-
-        //set frequency to 1 for new key and add to deque
-        keyFreqmap.put(key, 1);
-        freqListmap.computeIfAbsent(1, k -> new LinkedList<>()).addFirst(key);
-
-        //set least freq to 1
-        leastFreq = 1;
-    }
-
-    @Override
-    public String get(String key) {
-        if (!keyValmap.containsKey(key)) return null;
-
-        int prevFreq = keyFreqmap.get(key);
-        freqListmap.get(prevFreq).remove(key); //remove from prev deque
-        //update least frequency
-        if (freqListmap.get(prevFreq).isEmpty() && prevFreq == leastFreq) {
-            leastFreq++;
-        }
-
-        int newFreq = prevFreq + 1;
-        keyFreqmap.put(key, newFreq); //set new frequency
-        freqListmap.computeIfAbsent(newFreq, k -> new LinkedList<>()).addFirst(key); //promote to new freq list
-
-        return keyValmap.get(key);
-    }
-
-    @Override
-    public int size() {
-        return keyValmap.size();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return keyValmap.isEmpty();
-    }
-
-    @Override
-    public void state() {
-        System.out.println("Sate Start");
-        System.out.println(keyValmap);
-        System.out.println(keyFreqmap);
-        System.out.println(freqListmap);
-        System.out.println("Sate End");
-    }
-}
