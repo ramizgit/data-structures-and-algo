6c7c6f0bcb4a626e7db8e982936d1c7892b24af7
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-05-23 09:12:54 +0530

    Update DirectedGraphCycleDetection.java

--- src/graph/cyclepattern/DirectedGraphCycleDetection.java
+++ src/graph/cyclepattern/DirectedGraphCycleDetection.java
@@ -2,6 +2,24 @@ package graph.cyclepattern;
 
 import java.util.*;
 
+/*
+in directed graph, we can detect cycle in two ways
+    1. DFS approach - below impl is dfs approach
+    2. topological sorting (kahn's algo) - follow "CourseSchedule"
+*/
+
+/*
+important note, in DFS approach, only visited tracking alone is NOT sufficient for cycle detection in directed graph
+we also need current path node tracking
+
+for example : in below graph we visit 3 twice, one path 0 → 1 → 3 and another path 0 → 2 → 3. here we
+encounter 3 twice, but still no cycle.
+
+0 → 1 → 3
+ \       ↑
+  → 2 ───┘
+*/
+
 public class DirectedGraphCycleDetection {
 
     public boolean hasCycle(int n, int[][] edges)
