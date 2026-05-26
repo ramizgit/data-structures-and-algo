944545e8368578269d48fa70d7b37108160f2d85
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-05-26 22:23:21 +0530

    Update TimeBasedKeyValueStoreLeetcode.java

--- src/binarysearch/TimeBasedKeyValueStoreLeetcode.java
+++ src/binarysearch/TimeBasedKeyValueStoreLeetcode.java
@@ -1,4 +1,4 @@
-package binarysearch;
+package binarySearch;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -6,92 +6,69 @@ import java.util.List;
 import java.util.Map;
 
 public class TimeBasedKeyValueStoreLeetcode {
-    public static void main(String[] args)
-    {
-        TimeMap map = new TimeMap();
-        map.set("key", "val1", 2);
-        map.set("key", "val2", 5);
-        map.set("key", "val3", 10);
-        map.set("key", "val4", 15);
-        map.set("key", "val5", 20);
 
-        System.out.println(map.get("key", 1));//val1
-        System.out.println(map.get("key", 2));//val1
-        System.out.println(map.get("key", 3));//val1
-        System.out.println();
-        System.out.println(map.get("key", 5));//val2
-        System.out.println(map.get("key", 7));//val2
-        System.out.println(map.get("key", 9));//val2
-        System.out.println();
-        System.out.println(map.get("key", 10));//val3
-        System.out.println(map.get("key", 11));//val3
-        System.out.println(map.get("key", 14));//val3
-        System.out.println();
-        System.out.println(map.get("key", 15));//val4
-        System.out.println(map.get("key", 19));//val4
-        System.out.println();
-        System.out.println(map.get("key", 20));//val5
-        System.out.println(map.get("key", 21));//val5
-        System.out.println(map.get("key", 100));//val5
-    }
-}
+    class TimeBasedKeyValueStore {
 
-class TimeMap{
-    private Map<String, List<TimeValuePair>> map;
+        private Map<String, List<Value>> keyValuesMap;
 
-    public TimeMap()
-    {
-        this.map = new HashMap<>();
-    }
-
-    public void set(String key, String value, int timestamp)
-    {
-        this.map.computeIfAbsent(key, val -> new ArrayList<>()).add(new TimeValuePair(timestamp, value));
-    }
-
-    public String get(String key, int timestamp)
-    {
-        List<TimeValuePair> values = this.map.get(key);
-
-        if(values == null || values.isEmpty()){
-            return "";
+        public TimeBasedKeyValueStore()
+        {
+            this.keyValuesMap = new HashMap<>();
         }
 
-        //handle edge cases
-        if(timestamp <= values.get(0).time){
-            return ""; //no valid answer exists since requested timestamp is SMALLER than first timestamp
-        }
-        if(timestamp >= values.get(values.size()-1).time){
-            return values.get(values.size()-1).value;
+        //Time complexity : O(1)
+        public void set(String key, String value, int timestamp)
+        {
+            this.keyValuesMap.computeIfAbsent(key, val -> new ArrayList<>()).add(new Value(timestamp, value));
         }
 
-        //else do binary search
-        int low = 0;
-        int high = values.size()-1;
-        String result = "";
+        //Time complexity : O(logn)
+        public String get(String key, int timestamp)
+        {
+            List<Value> values = this.keyValuesMap.get(key);
 
-        while (low <= high){
-            int mid = low + (high - low)/2;
-
-            if (values.get(mid).time == timestamp) {
-                return values.get(mid).value;
-            } else if (values.get(mid).time < timestamp) {
-                result = values.get(mid).value; //potential answer, might be overriden if a more suitable time matching found later
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+            //handle edge cases
+            if(values == null || values.isEmpty()){
+                return "";
             }
+
+            if(timestamp < values.getFirst().time){
+                return ""; //no valid answer exists since requested timestamp is SMALLER than first timestamp
+            }
+
+            if(timestamp >= values.getLast().time){
+                return values.getLast().value;
+            }
+
+            //else do binary search on values list
+            int low = 0;
+            int high = values.size()-1;
+            String result = "";
+
+            while (low <= high){
+
+                int mid = low + (high - low)/2;
+
+                if (values.get(mid).time <= timestamp) {
+                    result = values.get(mid).value; //potential answer, might be overriden if a more suitable time matching found later
+                    low = mid + 1; //try higher timestamp
+                } else {
+                    high = mid - 1; //try lower timestamp
+                }
+            }
+            return result;
         }
-        return result;
+    }
+
+    class Value {
+        int time;
+        String value;
+
+        public Value(int time, String value) {
+            this.time = time;
+            this.value = value;
+        }
     }
 }
 
-class TimeValuePair{
-    int time;
-    String value;
 
-    public TimeValuePair(int time, String value) {
-        this.time = time;
-        this.value = value;
-    }
-}
