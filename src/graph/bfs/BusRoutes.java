4ce706d4fc0413ff2798ef5a0ce68a3d3d564c45
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-05-27 12:21:20 +0530

    Create BusRoutes.java

---
+++ src/graph/bfs/BusRoutes.java
@@ -0,0 +1,69 @@
+package graph.bfs;
+
+import java.util.*;
+
+public class BusRoutes {
+
+    //https://leetcode.com/problems/bus-routes/
+
+    public int numBusesToDestination(int[][] routes, int source, int target)
+    {
+        //edge case
+        if(source == target){
+            return 0;
+        }
+
+        //graph modelling
+        Map<Integer, List<Integer>> stopToRoutesMap = new HashMap<>();
+
+        for(int i=0; i<routes.length; i++){
+            for(int stop : routes[i]){
+                stopToRoutesMap.computeIfAbsent(stop, key -> new ArrayList<>()).add(i);
+            }
+        }
+
+        //bfs from source
+        Queue<State> bfsQueue = new ArrayDeque<>();
+        bfsQueue.add(new State(source, 0));
+
+        Set<Integer> visitedRoutes = new HashSet<>();
+        Set<Integer> visitedStops = new HashSet<>();
+        visitedStops.add(source); //starting bus stop
+
+        while(!bfsQueue.isEmpty()){
+            State curr = bfsQueue.poll();
+
+            //exit condition
+            if(curr.stop == target){
+                return curr.dist;
+            }
+
+            //explore neighbouring routes from the current stop
+            for(int route : stopToRoutesMap.getOrDefault(curr.stop, Collections.emptyList())){
+                if(!visitedRoutes.contains(route)){
+                    visitedRoutes.add(route);//add to visited
+
+                    //explore all routes in the current route
+                    for(int stop : routes[route]){
+                        if(!visitedStops.contains(stop)){
+                            bfsQueue.add(new State(stop, curr.dist + 1));
+                            visitedStops.add(stop);
+                        }
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    class State{
+        int stop;
+        int dist;
+
+        public State(int stop, int dist) {
+            this.stop = stop;
+            this.dist = dist;
+        }
+    }
+}
