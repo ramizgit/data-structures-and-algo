65ca39211324f111abe599957543d81ac4288b7d
ramizgit <65121963+ramizgit@users.noreply.github.com>
2026-05-27 12:40:31 +0530

    Update MaxSubArraySumEqualsK.java

--- src/prefixSum/MaxSubArraySumEqualsK.java
+++ src/prefixSum/MaxSubArraySumEqualsK.java
@@ -4,30 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MaxSubArraySumEqualsK {
+    
     //https://leetcode.com/problems/maximum-size-subarray-sum-equals-k/
-    public static void main(String[] args)
-    {
-        System.out.println(maxSubArray(new int[]{1,1,1,1,-1,-1,-1,-1,1,1,1,1,5}, 4));
-
-    }
-
+  
     private static int maxSubArray(int[] arr, int target)
     {
-        Map<Integer, Integer> prefixSum = new HashMap<>();
-        prefixSum.put(0, -1);
+       Map<Integer, Integer> sumIdxMap = new HashMap<>();
+        sumIdxMap.put(0, -1); //base case
         int sum = 0;
-        int maxLen = 0;
+        int max = 0;
 
-        for(int i=0; i<arr.length; i++){
-            sum += arr[i];
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
 
-            if(prefixSum.containsKey(sum - target)){
-                maxLen = Math.max(maxLen, (i - prefixSum.get(sum - target)));
+            if(sumIdxMap.containsKey(sum - k)){
+                max = Math.max(max, i - sumIdxMap.get(sum - k));
             }
 
-            prefixSum.putIfAbsent(sum, i);
+            // store "first" occurrence only for maximum sub array length
+            sumIdxMap.putIfAbsent(sum, i);
         }
 
-        return maxLen;
+        return max;
     }
 }
