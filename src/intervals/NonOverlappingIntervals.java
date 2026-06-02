0eddb3aedd99a54bdaeeb186447c4128020a4e26
Ramiz Raza <rramiz.rraza@gmail.com>
2026-06-02 16:56:59 +0530

    dsa code NonOverlappingIntervals

--- src/intervals/NonOverlappingIntervals.java
+++ src/intervals/NonOverlappingIntervals.java
@@ -11,14 +11,15 @@ public class NonOverlappingIntervals {
     return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
     Note that intervals which only touch at a point are non-overlapping. For example, [1, 2] and [2, 3] are non-overlapping.
      */
-    //Time : O(nlogn)
+
+    //option1 : sort by end time
     public int eraseOverlapIntervals(int[][] intervals)
     {
         if(intervals == null || intervals.length == 0){
             return 0;
         }
 
-        //Greedy approach : always keep the interval that ends earliest, it leaves maximum space for future intervals
+        //Greedy approach : always keep the interval that ends earliest, because it frees up the timeline earlier and leaves maximum space for future intervals
         Arrays.sort(intervals, (a, b) -> a[1] - b[1]); //sort by end time
 
         int remove = 0;
@@ -32,6 +33,33 @@ public class NonOverlappingIntervals {
                 remove++; //overlap found → remove current interval (greedy choice)
             }else{
                 prevEndTime = currentEndTime; //move forward with current valid interval
+            }
+        }
+
+        return remove;
+    }
+
+    //option2 : sort by start time
+    public int eraseOverlapIntervals2(int[][] intervals)
+    {
+        if(intervals == null || intervals.length == 0){
+            return 0;
+        }
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]); // sort by start time
+
+        int remove = 0;
+        int prevEndTime = intervals[0][1];
+
+        for(int i = 1; i < intervals.length; i++){
+            int currStartTime = intervals[i][0];
+            int currentEndTime = intervals[i][1];
+
+            if(currStartTime < prevEndTime){
+                remove++; // overlap found
+                prevEndTime = Math.min(prevEndTime, currentEndTime); // keep the interval that ends earlier
+            }else{
+                prevEndTime = currentEndTime;
             }
         }
 
