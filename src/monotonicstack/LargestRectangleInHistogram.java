e6ed4917f0e57d18564f88638ccfa60c8f9f27ae
Ramiz Raza <rramiz.rraza@gmail.com>
2026-06-03 16:39:15 +0530

    dsa code LargestRectangleInHistogram

--- src/monotonicstack/LargestRectangleInHistogram.java
+++ src/monotonicstack/LargestRectangleInHistogram.java
@@ -1,10 +1,10 @@
 package monotonicstack;
 
-import java.util.Stack;
+import java.util.*;
 
 public class LargestRectangleInHistogram {
 
-    public static int getLargestRectangle(int[] arr)
+    public static int getLargestRectangle(int[] heights)
     {
         /*
         Approach
@@ -15,49 +15,49 @@ public class LargestRectangleInHistogram {
             area = height * width
          */
 
-        int[] leftArray = computeLeftArray(arr);
-        int[] rightArray = computeRightArray(arr);
+        int[] leftArray = computeLeftBoundary(heights);
+        int[] rightArray = computeRightBoundary(heights);
 
         int maxArea = 0;
 
-        for(int i=0; i<arr.length; i++)
+        for(int i=0; i<heights.length; i++)
         {
-            int area = arr[i] * (rightArray[i] - leftArray[i] + 1);
+            int area = heights[i] * (rightArray[i] - leftArray[i] + 1);
             maxArea = Math.max(maxArea, area);
         }
 
         return maxArea;
     }
 
-    public static int[] computeLeftArray(int[] arr)
+    public static int[] computeLeftBoundary(int[] arr)
     {
         //next smaller element in left direction, use monotonic increasing stack
         int[] leftArray = new int[arr.length];
-        Stack<Integer> stack = new Stack<>();
+        Deque<Integer> stack = new ArrayDeque<>();
 
         for(int i=0; i<arr.length; i++){
-            while(!stack.empty() && arr[i] <= arr[stack.peek()]){
+            while(!stack.isEmpty() && arr[i] <= arr[stack.peek()]){
                 stack.pop();
             }
 
-            leftArray[i] = stack.empty() ? 0 : stack.peek()+1;
+            leftArray[i] = stack.isEmpty() ? 0 : stack.peek()+1;
             stack.push(i);
         }
         return leftArray;
     }
 
-    public static int[] computeRightArray(int[] arr)
+    public static int[] computeRightBoundary(int[] arr)
     {
         //next smaller element in right direction, use monotonic increasing stack
         int[] rightArray = new int[arr.length];
-        Stack<Integer> stack = new Stack<>();
+        Deque<Integer> stack = new ArrayDeque<>();
 
         for(int i=arr.length-1; i>=0; i--){
-            while(!stack.empty() && arr[i] <= arr[stack.peek()]){
+            while(!stack.isEmpty() && arr[i] <= arr[stack.peek()]){
                 stack.pop();
             }
 
-            rightArray[i] = stack.empty() ? arr.length-1 : stack.peek()-1;
+            rightArray[i] = stack.isEmpty() ? arr.length-1 : stack.peek()-1;
             stack.push(i);
         }
         return rightArray;
