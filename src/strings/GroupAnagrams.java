41eeb75c8d7f09d45315b236c1b84f929be945aa
Ramiz Raza <rramiz.rraza@gmail.com>
2026-06-03 08:04:35 +0530

    dsa code GroupAnagrams

--- src/strings/GroupAnagrams.java
+++ src/strings/GroupAnagrams.java
@@ -5,23 +5,17 @@ import java.util.*;
 public class GroupAnagrams {
 
     //https://leetcode.com/problems/group-anagrams/description/
-    public List<List<String>> groupAnagrams(String[] strs) {
-
-        Map<String, List<String>> map = new HashMap<>();
-
-        for(String str : strs){
-            char[] arr = str.toCharArray();
-            Arrays.sort(arr);
-            String key = new String(arr);
-            map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
-        }
-
-        return new ArrayList<>(map.values());
-    }
 
     /*USE FREQ ARR WITHOUT SORTING*/
-    public List<List<String>> groupAnagrams2(String[] strs) {
-
+    /*
+    Time complexity
+    N = number of strings
+    K = average string length
+    Building frequency array: O(k)
+    Total : O(N × K)
+     */
+    public List<List<String>> groupAnagrams(String[] strs)
+    {
         Map<String, List<String>> map = new HashMap<>();
 
         for(String str : strs){
@@ -34,7 +28,23 @@ public class GroupAnagrams {
             for(int f : freq){
                 sb.append(f).append("#");
             }
+
             String key = sb.toString();
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    //using sorting approach, but with higher time complexity : O(N × K log K)
+    public List<List<String>> groupAnagrams2(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String str : strs){
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
             map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
         }
 
