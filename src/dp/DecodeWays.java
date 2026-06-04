e46c0f507e6e39b19e5e51e488326bcc70a9e249
Ramiz Raza <rramiz.rraza@gmail.com>
2026-06-04 19:53:39 +0530

    dsa code DecodeWays

--- src/dp/DecodeWays.java
+++ src/dp/DecodeWays.java
@@ -1,26 +1,20 @@
 package dp;
 
 public class DecodeWays {
-    //https://leetcode.com/problems/decode-ways/description/
-    public static void main(String[] args)
-    {
-        System.out.println(numDecodings("12"));
-        System.out.println(numDecodings("226"));
-        System.out.println(numDecodings("061"));
-        System.out.println(numDecodings("10011"));
-    }
 
-    private static int numDecodings(String input)
+    //https://leetcode.com/problems/decode-ways/description/
+
+    public int numDecodings(String input)
     {
         int n = input.length();
-        int[] dp = new int[n+1];
-        dp[0] = 1;
+        int[] dp = new int[n+1]; //dp[i] is the number of ways to decode the first i characters of the string
+        dp[0] = 1; //base case - empty string has only one way of valid decoding
         dp[1] = input.charAt(0) == '0' ? 0 : 1;
 
         for(int i=2; i<=n; i++){
-            int oneDigit = Integer.parseInt(input.substring(i-1, i)); //input.charAt(i-1) - '0';
+            int oneDigit = Integer.parseInt(input.substring(i-1, i));
             if(oneDigit >= 1){
-                dp[i] = dp[i-1];
+                dp[i] += dp[i-1];
             }
 
             int twoDigits = Integer.parseInt(input.substring(i-2, i));
