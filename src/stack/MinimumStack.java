cd4d28fc3f16025a5a445e9af6f1be37aa342efb
Ramiz Raza <rramiz.rraza@gmail.com>
2026-06-04 19:28:38 +0530

    dsa code MinimumStack

--- src/stack/MinimumStack.java
+++ src/stack/MinimumStack.java
@@ -5,54 +5,42 @@ import java.util.Deque;
 
 public class MinimumStack {
 
-    public static void main(String[] args)
-    {
-        MinStack minStack = new MinStack();
-        minStack.push(1);
-        minStack.push(2);
-        minStack.push(0);
-        System.out.println(minStack.getMin()); // return 0
-        minStack.pop();
-        System.out.println(minStack.top()); // return 2
-        System.out.println(minStack.getMin()); // return 1
-    }
-}
+    class MinStack {
 
-class MinStack {
+        //main stack that stores all values
+        Deque<Integer> stack;
 
-    //main stack that stores all values
-    Deque<Integer> stack;
+        //min stack that maintains the minimum value at each corresponding level
+        Deque<Integer> minStack;
 
-    //min stack that maintains the minimum value at each corresponding level
-    Deque<Integer> min;
-
-    public MinStack() {
-        stack = new ArrayDeque<>();
-        min = new ArrayDeque<>();
-    }
-
-    public void push(int val) {
-        stack.push(val);
-
-        if (min.isEmpty()) {
-            min.push(val);
-        } else {
-            min.push(Math.min(val, min.peek()));
+        public MinStack() {
+            stack = new ArrayDeque<>();
+            minStack = new ArrayDeque<>();
         }
-    }
 
-    public void pop() {
-        if(!stack.isEmpty()){
-            stack.pop();
-            min.pop();
+        public void push(int val) {
+            stack.push(val);
+
+            if (minStack.isEmpty()) {
+                minStack.push(val);
+            } else {
+                minStack.push(Math.min(val, minStack.peek()));
+            }
         }
-    }
 
-    public int top() {
-        return stack.peek();
-    }
+        public void pop() {
+            if(!stack.isEmpty()){
+                stack.pop();
+                minStack.pop();
+            }
+        }
 
-    public int getMin() {
-        return min.peek();
+        public int top() {
+            return stack.peek();
+        }
+
+        public int getMin() {
+            return minStack.peek();
+        }
     }
 }
