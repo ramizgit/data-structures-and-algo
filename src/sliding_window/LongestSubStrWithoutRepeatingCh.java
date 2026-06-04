5a4b0517a60f837ee1b19088005d3169d4daec5f
Ramiz Raza <rramiz.rraza@gmail.com>
2026-06-04 19:37:16 +0530

    dsa code LongestSubStrWithoutRepeatingCh

--- src/sliding_window/LongestSubStrWithoutRepeatingCh.java
+++ src/sliding_window/LongestSubStrWithoutRepeatingCh.java
@@ -5,36 +5,34 @@ import java.util.Set;
 
 public class LongestSubStrWithoutRepeatingCh {
 
-    public static void main(String[] args)
-    {
-        System.out.println(lengthOfLongestSubstring("abcda")); //4
-        System.out.println(lengthOfLongestSubstring("abcabcbb")); //3
-        System.out.println(lengthOfLongestSubstring("pwwkewrtw")); //5
-        System.out.println(lengthOfLongestSubstring("bbbbb")); //1
-    }
+    //https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
 
     private static int lengthOfLongestSubstring(String s)
     {
+        //input validation
         if(s == null || s.isEmpty()){
             return 0;
         }
 
+        //sliding window
         int left = 0;
         int right = 0;
         int maxLen = 0;
-        Set<Character> set = new HashSet<>();
+        Set<Character> windowSet = new HashSet<>();
 
         while(right < s.length()){
-            char ch = s.charAt(right);
 
-            while(set.contains(ch)){
-                set.remove(s.charAt(left));
+            char current = s.charAt(right);
+
+            while(windowSet.contains(current)){
+                //shrink window
+                windowSet.remove(s.charAt(left));
                 left++;
             }
 
-            set.add(ch);
+            windowSet.add(current); //expand window
 
-            maxLen = Math.max(maxLen, right - left + 1);
+            maxLen = Math.max(maxLen, right - left + 1); //capture max window length
 
             right++;
         }
