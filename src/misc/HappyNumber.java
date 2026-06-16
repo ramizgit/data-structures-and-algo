dd600c6ecf8a44968b6c8f310ce9241c0a9ebfcd
Ramiz Raza <rramiz.rraza@gmail.com>
2026-06-16 10:38:44 +0530

    dsa code HappyNumber

--- src/misc/HappyNumber.java
+++ src/misc/HappyNumber.java
@@ -4,21 +4,21 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class HappyNumber {
+
     //https://leetcode.com/problems/happy-number/description/
-    public static void main(String[] args)
-    {
-        System.out.println(isHappy(19));
-        System.out.println(isHappy(2));
-    }
 
     private static boolean isHappy(int n)
     {
-        Set<Integer> seen = new HashSet<>();
+        Set<Integer> seen = new HashSet<>(); //to detect cycle
+
         while (true){
+
             int sum = 0;
+
             while (n != 0){
-                sum += Math.pow(n%10, 2);
-                n = n / 10;
+                int lastDigit = n % 10; //extract last digit
+                sum += lastDigit * lastDigit;
+                n = n / 10; //continue with remaining number
             }
 
             if(sum == 1){
