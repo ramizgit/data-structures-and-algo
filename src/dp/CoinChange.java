b14f2467a750c3a27033ed29d9b4c58be03c1395
Ramiz Raza <rramiz.rraza@gmail.com>
2026-06-16 07:06:12 +0530

    dsa code CoinChange

--- src/dp/CoinChange.java
+++ src/dp/CoinChange.java
@@ -13,6 +13,26 @@ public class CoinChange {
 
         dp[0] = 0; //base case - 0 coins needed to make amount 0
 
+        for(int c : coins){
+            for(int i=c; i<=amount; i++){ //forward loop as coins can be reused
+                dp[i] = Math.min(
+                        dp[i], //dont pick
+                        1 + dp[i-c] //pick
+                );
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    private static int minCoinChange2(int[] coins, int amount)
+    {
+        int[] dp = new int[amount+1]; // dp[i] = minimum coins needed to make amount i
+
+        Arrays.fill(dp,amount+1); //initalize with some large value
+
+        dp[0] = 0; //base case - 0 coins needed to make amount 0
+
         for(int i=1; i<=amount; i++){ // build answers from smaller amounts till the target amount
             for(int c : coins){ // try every coin
                 if(i >= c){
