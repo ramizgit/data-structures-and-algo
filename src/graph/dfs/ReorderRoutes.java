67b5e729047df14653d522ef14a17e487aad1bbd
Ramiz Raza <rramiz.rraza@gmail.com>
2026-06-21 19:46:09 +0530

    dsa code ReorderRoutes

--- src/graph/dfs/ReorderRoutes.java
+++ src/graph/dfs/ReorderRoutes.java
@@ -14,6 +14,14 @@ public class ReorderRoutes {
      Return the minimum number of edges changed.
      */
 
+    /*
+    Approach:-
+    During DFS from city 0, we always move away from city 0. If the original road already points toward city 0 (a "friendly" edge),
+    DFS will traverse the artificial reverse edge with cost 0, meaning no reversal is needed.
+    If the original road points away from city 0 (an "unfriendly" edge), DFS will traverse the original edge with cost 1, meaning this road must be reversed.
+    By summing the costs of all traversed edges, we get the minimum number of reversals.
+     */
+
     public int minReorder(int n, int[][] connections)
     {
         //initialize graph
@@ -28,7 +36,7 @@ public class ReorderRoutes {
             int v = edge[1];
 
             graph.get(u).add(new Edge(v, 1)); //original edges with cost 1, needs reversal, wrong direction (away from 0)
-            graph.get(v).add(new Edge(u, 0)); //reverse edges with cost 0, correct direction (toward 0), hence cost 0
+            graph.get(v).add(new Edge(u, 0)); //artificial reverse edges with cost 0, correct direction (toward 0), hence cost 0
         }
 
         //run dfs from node 0
@@ -38,15 +46,14 @@ public class ReorderRoutes {
         return cost[0];
     }
 
+    //since graph is tree, hence using parent rather than visited array approach, this is more space efficient O(1)
     private void dfsRoot(int node, int parent, int[] cost, Map<Integer, List<Edge>> graph)
     {
         //explore neighborus
         for(Edge neighbour : graph.get(node)){
-            int v = neighbour.v;
-
-            if(v != parent){ //ensures recursion only goes to children, not back to parent to avoid infinite loop
+            if(neighbour.v != parent){ //ensures recursion only goes to children, not back to parent to avoid infinite loop
                 cost[0] += neighbour.w;
-                dfsRoot(v, node, cost, graph);
+                dfsRoot(neighbour.v, node, cost, graph);
             }
         }
     }
