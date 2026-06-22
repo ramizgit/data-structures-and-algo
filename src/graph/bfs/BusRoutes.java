331df71d4728a8583cf7d591c58dfea9366aae7e
Ramiz Raza <rramiz.rraza@gmail.com>
2026-06-22 14:30:21 +0530

    dsa code BusRoutes

--- src/graph/bfs/BusRoutes.java
+++ src/graph/bfs/BusRoutes.java
@@ -22,28 +22,29 @@ public class BusRoutes {
             }
         }
 
-        //bfs from source
+        //bfs from source stop
         Queue<State> bfsQueue = new ArrayDeque<>();
-        bfsQueue.add(new State(source, 0));
+        bfsQueue.add(new State(source, 0)); //starting bus stop
 
-        Set<Integer> visitedRoutes = new HashSet<>();
         Set<Integer> visitedStops = new HashSet<>();
         visitedStops.add(source); //starting bus stop
+        Set<Integer> visitedRoutes = new HashSet<>();
 
         while(!bfsQueue.isEmpty()){
+
             State currStop = bfsQueue.poll();
 
             //exit condition
             if(currStop.stop == target){
-                return currStop.dist;
+                return currStop.dist; //target reached
             }
 
             //explore neighbouring routes from the current stop
             for(int route : stopToRoutesMap.getOrDefault(currStop.stop, Collections.emptyList())){
                 if(!visitedRoutes.contains(route)){
-                    visitedRoutes.add(route);//add to visited
+                    visitedRoutes.add(route);//mark this route visited and add all its stops in the bfs queue in the next step
 
-                    //explore all routes in the current route
+                    //explore all stops in the current route
                     for(int stop : routes[route]){
                         if(!visitedStops.contains(stop)){
                             bfsQueue.add(new State(stop, currStop.dist + 1));
@@ -57,7 +58,7 @@ public class BusRoutes {
         return -1;
     }
 
-    class State{
+    static class State{
         int stop;
         int dist;
 
