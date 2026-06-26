d4215d82ec085792d1ef4ae715ef2ef81edca9f1
Ramiz Raza <rramiz.rraza@gmail.com>
2026-06-26 20:04:56 +0530

    dsa code refactor

---
+++ src/grid/dfs/NumberOfClosedIslands.java
@@ -0,0 +1,13 @@
+package consistenthashing.grid.dfs;
+
+public class NumberOfClosedIslands {
+
+    //https://leetcode.com/problems/number-of-closed-islands/description/
+
+    //todo : implement
+
+    public int closedIsland(int[][] grid)
+    {
+        return 0;
+    }
+}
--- src/grid/dfs/SurroundedRegionsDFS.java
+++ src/grid/dfs/SurroundedRegionsDFS.java
@@ -1,4 +1,4 @@
-package matrix;
+package consistenthashing.grid.dfs;
 
 public class SurroundedRegionsDFS {
 
