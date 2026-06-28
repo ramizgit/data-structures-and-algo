1a395fc9fc69c9adbdb23a3276f8671100922f11
Ramiz Raza <rramiz.rraza@gmail.com>
2026-06-28 19:37:30 +0530

    dsa code line sweep and interval CheckIfAllIntegersInRangeAreCovered

---
+++ src/lineSweep/CheckIfAllIntegersInRangeAreCovered.java
@@ -0,0 +1,47 @@
+package consistenthashing.lineSweep;
+
+public class CheckIfAllIntegersInRangeAreCovered {
+
+    //https://leetcode.com/problems/check-if-all-the-integers-in-a-range-are-covered/description/
+
+    /*
+    1 <= ranges.length <= 50
+    1 <= starti <= endi <= 50
+    1 <= left <= right <= 50
+     */
+
+    // Time : O(n)
+    // Space: O(1)
+    public boolean isCovered(int[][] ranges, int left, int right)
+    {
+        //input validation
+        if (ranges == null || ranges.length == 0) {
+            return false;
+        }
+
+        //conver each range into difference array update using start (+1) and end (-1)
+        int[] diff = new int[52];
+
+        for(int[] range : ranges){
+
+            int start = range[0];
+            int end = range[1];
+
+            diff[start] += 1;
+            diff[end+1] -= 1;
+        }
+
+        //sweep through the difference array using prefix sum
+        for(int i=1; i<diff.length; i++){
+            diff[i] += diff[i-1];
+        }
+
+        for(int i=left; i<=right; i++){
+            if(diff[i] == 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
