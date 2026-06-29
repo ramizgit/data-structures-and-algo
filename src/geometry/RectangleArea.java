7f0de2db13946552c5740d1f20358d309318e0bb
Ramiz Raza <rramiz.rraza@gmail.com>
2026-06-29 06:47:22 +0530

    dsa code RectangleArea

--- src/geometry/RectangleArea.java
+++ src/geometry/RectangleArea.java
@@ -16,10 +16,10 @@ public class RectangleArea {
         // Area of second rectangle.
         int area2 = (bx2 - bx1) * (by2 - by1);
 
-        // Find overlap on the x-axis.
+        // Find overlap on the x-axis. Negative means no overlap, hence make it zero
         int overlapWidth = Math.max(0, Math.min(ax2, bx2) - Math.max(ax1, bx1));
 
-        // Find overlap on the y-axis.
+        // Find overlap on the y-axis. Negative means no overlap, hence make it zero
         int overlapHeight = Math.max(0, Math.min(ay2, by2) - Math.max(ay1, by1));
 
         // Overlap area.
