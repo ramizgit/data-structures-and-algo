88ec9319461e45d355dcd7987c17b6a713c55ed2
Ramiz Raza <rramiz.rraza@gmail.com>
2026-06-29 18:00:39 +0530

    dsa code refactor CoinChange

--- src/dp/CoinChange.java
+++ src/dp/CoinChange.java
@@ -9,7 +9,7 @@ public class CoinChange {
     {
         int[] dp = new int[amount+1]; // dp[i] = minimum coins needed to make amount i
 
-        Arrays.fill(dp,amount+1); //initalize with some large value
+        Arrays.fill(dp,amount+1); //initialize with some large value
 
         dp[0] = 0; //base case - 0 coins needed to make amount 0
 
@@ -25,29 +25,6 @@ public class CoinChange {
         return dp[amount] > amount ? -1 : dp[amount];
     }
 
-    private static int minCoinChange2(int[] coins, int amount)
-    {
-        int[] dp = new int[amount+1]; // dp[i] = minimum coins needed to make amount i
-
-        Arrays.fill(dp,amount+1); //initalize with some large value
-
-        dp[0] = 0; //base case - 0 coins needed to make amount 0
-
-        for(int i=1; i<=amount; i++){ // build answers from smaller amounts till the target amount
-            for(int c : coins){ // try every coin
-                if(i >= c){
-                    dp[i] = Math.min(
-                            dp[i], //dont pick
-                            1 + dp[i-c] //pick
-                    );
-                }
-
-            }
-        }
-
-        return dp[amount] > amount ? -1 : dp[amount];
-    }
-
     //NOTE : ALSO PRINTS COINS USED
     private static int minCoinChangeAlongWithCoinsUsed(int[] coins, int amount)
     {
@@ -55,16 +32,21 @@ public class CoinChange {
         Arrays.fill(dp,amount+1);
         dp[0] = 0; //base case
 
-        int[] choice = new int[amount+1];
+        int[] choice = new int[amount+1]; // choice[i] stores the last coin used to form amount i
 
-        for(int i=1; i<=amount; i++){
-            for(int c : coins){
-                if( (i >= c) && ( (1 + dp[i-c]) < dp[i]) ){
+        // Unbounded Knapsack
+        for(int c : coins){
+            for(int i=c; i<=amount; i++){ //forward loop as coins can be reused
+                if( (1 + dp[i-c]) < dp[i]){
                     dp[i] = 1 + dp[i-c];
                     choice[i] = c;
                 }
-
             }
+        }
+
+        // No solution exists
+        if (dp[amount] > amount) {
+            return -1;
         }
 
         //PRINT COINS
@@ -73,12 +55,12 @@ public class CoinChange {
 
         while(curr > 0){
             int coin = choice[curr];
-            curr -= coin;
             coinsUsed.add(coin);
+            curr -= coin;
         }
 
         System.out.println("coins used : " + coinsUsed);
 
-        return dp[amount] > amount ? -1 : dp[amount];
+        return dp[amount];
     }
 }
