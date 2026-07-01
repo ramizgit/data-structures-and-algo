11ff57453cab330562a132f8069559c54a7cdc4b
Ramiz Raza <rramiz.rraza@gmail.com>
2026-07-01 10:00:00 +0530

    dsa code dp TargetSum

---
+++ src/dp/TargetSum.java
@@ -0,0 +1,54 @@
+package consistenthashing.dp;
+
+public class TargetSum {
+
+    //https://leetcode.com/problems/target-sum/description/
+
+    /*
+    // Let P = sum of numbers assigned '+' (+ve group)
+    // Let N = sum of numbers assigned '-' (-ve group)
+    //
+    // P - N = target
+    // P + N = totalSum
+    //
+    // => 2P = totalSum + target
+    // => P = (totalSum + target) / 2
+    //
+    // So the problem reduces to:
+    // Count subsets whose sum is (totalSum + target) / 2.
+     */
+
+    public int findTargetSumWays(int[] nums, int target)
+    {
+        int totalSum = 0;
+
+        for(int num : nums){
+            totalSum += num;
+        }
+
+        if (Math.abs(target) > totalSum)
+            return 0;
+
+        if ((totalSum + target) % 2 != 0)
+            return 0;
+
+        int requiredSum = (totalSum + target) / 2;
+
+        return countSubsetsWithSumK(nums, requiredSum);
+    }
+
+    private int countSubsetsWithSumK(int[] nums, int target)
+    {
+        int[] dp = new int[target + 1];
+
+        dp[0] = 1; // One way to make sum 0: choose no elements
+
+        for (int n : nums) {
+            for (int t = target; t >= n; t--) { // Backward loop since each element can be used at most once
+                dp[t] += dp[t - n];
+            }
+        }
+
+        return dp[target];
+    }
+}
