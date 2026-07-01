836d7416ae3bbaa2192a6958472d718a2e81a578
Ramiz Raza <rramiz.rraza@gmail.com>
2026-07-01 09:32:34 +0530

    dsa code dp refactor MinimumFallingPathSum

--- src/dp/MinimumFallingPathSum.java
+++ src/dp/MinimumFallingPathSum.java
@@ -1,14 +1,10 @@
 package dp;
 
 public class MinimumFallingPathSum {
+
     //https://leetcode.com/problems/minimum-falling-path-sum/description/
 
-    public static void main(String[] args)
-    {
-
-    }
-
-    private static int minFallingPathSum(int[][] matrix)
+    public int minFallingPathSum(int[][] matrix)
     {
         int n = matrix.length;
 
@@ -16,6 +12,7 @@ public class MinimumFallingPathSum {
         int[] dp = matrix[n-1].clone();
 
         for(int i=n-2; i>=0; i--){
+
             int[] curr = matrix[i];
 
             for(int j=0; j<n; j++){
