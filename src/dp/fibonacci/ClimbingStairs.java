bf9328161df19acaa61c0abc3e4be61464632303
Ramiz Raza <rramiz.rraza@gmail.com>
2026-07-02 08:37:01 +0530

    dsa code ClimbingStairs

--- src/dp/fibonacci/ClimbingStairs.java
+++ src/dp/fibonacci/ClimbingStairs.java
@@ -1,36 +1,27 @@
 package consistenthashing.dp.fibonacci;
 
 public class ClimbingStairs {
-    public static void main(String[] args)
-    {
-        System.out.println(climbStairs(1));
-        System.out.println(climbStairs(2));
-        System.out.println(climbStairs(3));
-        System.out.println(climbStairs(4));
-        System.out.println(climbStairs(5));
 
-        System.out.println("=========================");
+    //https://leetcode.com/problems/climbing-stairs/description/
 
-        System.out.println(climbStairs2(1));
-        System.out.println(climbStairs2(2));
-        System.out.println(climbStairs2(3));
-        System.out.println(climbStairs2(4));
-        System.out.println(climbStairs2(5));
-    }
-
+    //Time : O(n)
+    //Space : O(n)
     private static int climbStairs(int n)
     {
         if(n <= 2){
             return n;
         }
 
-        int[] dp = new int[n+1];
-        dp[0] = 0;
+        int[] dp = new int[n+1]; //dp[i] = number of distinct ways to reach step i
+
+        //base cases
         dp[1] = 1;
         dp[2] = 2;
 
+        //populate rest of dp
         for(int i=3; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            dp[i] = dp[i-1] //prev step
+                    + dp[i-2]; //two steps behind
         }
 
         return dp[n];
@@ -42,15 +33,15 @@ public class ClimbingStairs {
             return n;
         }
 
-        int minusOne = 2;
-        int minusTwo = 1;
+        int prev2 = 1;
+        int prev1 = 2;
         int result = 0;
 
         for(int i=3; i<=n; i++){
-            result = minusOne + minusTwo;
+            result = prev1 + prev2;
 
-            minusTwo = minusOne;
-            minusOne = result;
+            prev2 = prev1;
+            prev1 = result;
         }
 
         return result;
