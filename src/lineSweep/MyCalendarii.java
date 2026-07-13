10d030214b5394dd65689ba5e09146233ec346a4
Ramiz Raza <rramiz.rraza@gmail.com>
2026-07-13 10:56:31 +0530

    dsa code refactor

--- src/lineSweep/MyCalendarii.java
+++ src/lineSweep/MyCalendarii.java
@@ -6,8 +6,15 @@ public class MyCalendarii {
 
     //https://leetcode.com/problems/my-calendar-ii/description/
 
-    private static final int START = 1;
-    private static final int END = -1;
+    /*
+    "In My Calendar I, I can determine validity by checking only the predecessor and successor intervals, so I validate before inserting.
+    In My Calendar II, whether a booking is valid depends on the global maximum overlap after the new booking is added.
+    The easiest way to evaluate that is to temporarily insert its start and end events into the difference map, perform the sweep,
+    and roll back if the active booking count reaches 3."
+     */
+
+    private static final int START_DELTA = 1;
+    private static final int END_DELTA = -1;
 
     // Difference map : +1 at booking start and -1 at booking end
     TreeMap<Integer, Integer> events;
@@ -18,24 +25,25 @@ public class MyCalendarii {
 
     public boolean book(int startTime, int endTime)
     {
-        this.events.put(startTime, this.events.getOrDefault(startTime, 0) + START); //O(log n)
-        this.events.put(endTime, this.events.getOrDefault(endTime, 0) + END); //O(log n)
+        this.events.put(startTime, this.events.getOrDefault(startTime, 0) + START_DELTA); //O(log n)
+        this.events.put(endTime, this.events.getOrDefault(endTime, 0) + END_DELTA); //O(log n)
 
         int currActive = 0;
 
         for(int delta : events.values()){ //O(n)
+
             currActive += delta;
 
             if(currActive >= 3){
 
                 //rollback logic
-                events.put(startTime, events.get(startTime) - START); //rollback start
+                events.put(startTime, events.get(startTime) - START_DELTA); //rollback start
 
                 if (events.get(startTime) == 0) {
                     events.remove(startTime);
                 }
 
-                events.put(endTime, events.get(endTime) - END); //rollback end
+                events.put(endTime, events.get(endTime) - END_DELTA); //rollback end
 
                 if (events.get(endTime) == 0) {
                     events.remove(endTime);
