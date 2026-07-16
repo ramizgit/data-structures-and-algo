68f2b019f9d6c5997d4fb553911e032cf95490b4
Ramiz Raza <rramiz.rraza@gmail.com>
2026-07-16 20:00:25 +0530

    dsa code refactor

---
+++ src/intervals/MaxNumOfEventsThatCanBeAttended.java
@@ -0,0 +1,14 @@
+package intervals;
+
+public class MaxNumOfEventsThatCanBeAttended {
+
+    //https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended/description/
+
+    //todo : implement
+
+    public int maxEvents(int[][] events)
+    {
+
+        return 0;
+    }
+}
