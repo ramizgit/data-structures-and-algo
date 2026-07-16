d888779106e0357a46f86db607052b0d17f77f95
Ramiz Raza <rramiz.rraza@gmail.com>
2026-07-16 17:07:40 +0530

    dsa code refactor

--- src/array/DivideArrInSetsOfKConsecutiveNums.java
+++ src/array/DivideArrInSetsOfKConsecutiveNums.java
@@ -14,20 +14,27 @@ public class DivideArrInSetsOfKConsecutiveNums {
             return false;
         }
 
-        //build freq map
+        //build freq map {num, freq}
         TreeMap<Integer, Integer> map = new TreeMap<>();
+
         for(int num : nums){ //0(nlogn)
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
         while(!map.isEmpty()){
+
             int start = map.firstKey(); //greedy approach
+
             for(int i=0; i<k; i++){ //0(klogn)
+
                 int curr = start + i;
+
                 if(!map.containsKey(curr)){
                     return false;
                 }
+
                 map.put(curr, map.get(curr) - 1);
+
                 if(map.get(curr) == 0){
                     map.remove(curr);
                 }
