8f48e9ab706a4a0fff7d0e4d3799330400701a88
Ramiz Raza <rramiz.rraza@gmail.com>
2026-07-16 15:54:31 +0530

    dsa code refactor

--- src/array/twoDdominancequery/CarFleet.java
+++ src/array/twoDdominancequery/CarFleet.java
@@ -11,30 +11,40 @@ public class CarFleet {
         //target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3]
 
         //calculate time to reach for each car, and create list of car pair: {position, timeToReach}
-        List<double[]> cars = new ArrayList<>(); //list of cars pair: {position, timeToReach}
+        List<Car> cars = new ArrayList<>(); //list of cars pair: {position, timeToReach}
 
         for(int i=0; i<position.length; i++){
             int distance = target - position[i];
             double time = (double) distance / speed[i];
 
-            cars.add(new double[]{position[i], time});
+            cars.add(new Car(position[i], time));
         }
 
         //sort by start position in descending order
-        cars.sort( (a, b) -> (int) (b[0] - a[0]));
+        cars.sort( (a, b) -> Integer.compare(b.position, a.position));
 
         //traverse list and keep track of prev max time, new fleet only when curr time is higher than prev max time
-        double maxTime = 0;
+        double maxTimeSoFar = 0;
         int fleet = 0;
 
-        for(int i=0; i<cars.size(); i++){
-            double currTime = cars.get(i)[1];
-            if(currTime > maxTime){
+        for (Car car : cars) {
+            if (car.time > maxTimeSoFar) {
+                //reaches the target later than all fleets ahead, so it starts a new fleet.
                 fleet++; //new fleet
-                maxTime = currTime; //reset max time
+                maxTimeSoFar = car.time; //reset max time to current max
             }
         }
 
         return fleet;
+    }
+
+    static class Car {
+        int position;
+        double time;
+
+        public Car(int position, double time) {
+            this.position = position;
+            this.time = time;
+        }
     }
 }
