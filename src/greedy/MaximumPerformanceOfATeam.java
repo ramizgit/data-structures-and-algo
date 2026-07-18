5bb8c3bc98b333014919135dd87f251c26f5532d
Ramiz Raza <rramiz.rraza@gmail.com>
2026-07-18 20:14:58 +0530

    dsa code FurthestBuildingYouCanReach

---
+++ src/greedy/MaximumPerformanceOfATeam.java
@@ -0,0 +1,14 @@
+package greedy;
+
+public class MaximumPerformanceOfATeam {
+
+    //https://leetcode.com/problems/maximum-performance-of-a-team/description/
+
+    //todo : implement
+
+    public int maxPerformance(int n, int[] speed, int[] efficiency, int k)
+    {
+
+        return 0;
+    }
+}
