03287c0ddaefeb537111148e4828a5326389e4a7
Ramiz Raza <rramiz.rraza@gmail.com>
2026-07-18 15:30:01 +0530

    dsa code MinimumNumberOfRefuelingStops

--- src/greedy/MinimumNumberOfRefuelingStops.java
+++ src/greedy/MinimumNumberOfRefuelingStops.java
@@ -1,23 +1,62 @@
 package greedy;
 
+import java.util.*;
+
 public class MinimumNumberOfRefuelingStops {
 
-    //https://leetcode.com/problems/minimum-number-of-refueling-stops/description/
+    // https://leetcode.com/problems/minimum-number-of-refueling-stops/
 
-    public int minRefuelStops(int target, int startFuel, int[][] stations)
-    {
+    //todo : practice
+
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+
         /*
-        Stations: (position, fuel)
-          ↓
-        Process stations in order
-                  ↓
-        Store available fuel in max heap
-                  ↓
-        Refuel only when necessary
-         */
+        Greedy Idea:
 
-        //todo : implement
+        Stations are processed in increasing order of position.
 
-        return 0;
+        1. Keep all reachable stations in a max heap.
+        2. Only refuel when the next station (or target) is unreachable.
+        3. When refueling, always choose the station with the maximum fuel.
+        */
+
+        int answer = 0;
+
+        PriorityQueue<Integer> fuelMaxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+
+        int capacity = startFuel; // Maximum distance we can currently reach
+
+        for (int i = 0; i < stations.length; i++) {
+
+            int position = stations[i][0];
+            int fuel = stations[i][1];
+
+            //can't reach this station yet, keep refueling until we can or run out of options.
+            while (capacity < position) {
+
+                if (fuelMaxHeap.isEmpty()) {
+                    return -1;
+                }
+
+                capacity += fuelMaxHeap.poll();
+                answer++;
+            }
+
+            // This station is now reachable.
+            fuelMaxHeap.offer(fuel);
+        }
+
+        // After all stations, keep refueling until we reach the target.
+        while (capacity < target) {
+
+            if (fuelMaxHeap.isEmpty()) {
+                return -1;
+            }
+
+            capacity += fuelMaxHeap.poll();
+            answer++;
+        }
+
+        return answer;
     }
 }
