15084b739c96b4dbceeb2228b241a6f2f2da9c2d
Ramiz Raza <rramiz.rraza@gmail.com>
2026-07-22 18:22:18 +0530

    dsa code refactor

--- src/sliding_window/MaximumAverageSubarray.java
+++ src/sliding_window/MaximumAverageSubarray.java
@@ -1,32 +1,34 @@
 package slidingwindow;
 
 public class MaximumAverageSubarray {
+
     //https://leetcode.com/problems/maximum-average-subarray-i/description/
-    public static void main(String[] args)
-    {
-        System.out.println(findMaxAverage(new int[]{1,12,-5,-6,50,3}, 4));
-    }
 
     private static double findMaxAverage(int[] nums, int k)
     {
+        //build first window
         int sum = 0;
         for(int i=0; i<k; i++){
             sum += nums[i];
         }
 
-        double maxAvg = (double) sum / k;
+        int maxSum = sum;
 
         int left = 0;
         int right = k;
 
+        //slide remaining windows
         while (right < nums.length){
-            sum += nums[right] - nums[left];
-            maxAvg = Math.max(maxAvg, (double)sum/k);
 
-            right++;
+            sum += nums[right];
+            sum -= nums[left];
+
+            maxSum = Math.max(maxSum, sum);
+
             left++;
+            right++;
         }
 
-        return maxAvg;
+        return (double) maxSum / k;
     }
 }
