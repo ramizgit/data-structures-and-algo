3c584e64a310f1d23909595c11dc602572b2ac27
Ramiz Raza <rramiz.rraza@gmail.com>
2026-07-24 07:23:41 +0530

    dsa code CuttingRibbons

--- src/binarysearch/CuttingRibbons.java
+++ src/binarysearch/CuttingRibbons.java
@@ -19,8 +19,51 @@ public class CuttingRibbons {
 
     public int maxLength(int[] ribbons, int k)
     {
-        //todo : implement
+        //input validation
+        if(ribbons == null || ribbons.length == 0){
+            return 0;
+        }
 
-        return 0;
+        if(k <= 0){
+            return 0;
+        }
+
+        int max = 0;
+        for(int ribbon : ribbons){
+            max = Math.max(max, ribbon);
+        }
+
+        int low = 1; //minimum possible piece length
+        int high = max; //cannot make a piece longer than the longest ribbon
+        int answer = 0;
+
+        while(low <= high){
+
+            int mid = low + (high - low)/2;
+
+            if(feasibleToCut(ribbons, mid, k)){
+                answer = mid; //possible answer
+                low = mid + 1; //try higher to maximize the answer
+            }else{
+                high = mid - 1; //not feasible to cut, try lower
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean feasibleToCut(int[] ribbons, int len, int k)
+    {
+        long count = 0;
+
+        for (int ribbon : ribbons) {
+            count += ribbon / len;
+
+            if (count >= k) {
+                return true; //early exit
+            }
+        }
+
+        return false;
     }
 }
