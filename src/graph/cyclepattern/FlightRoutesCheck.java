0da95c86f802b98e278939640ddccd1e63f95ce2
Ramiz Raza <rramiz.rraza@gmail.com>
2026-07-27 18:10:07 +0530

    dsa code refactor

---
+++ src/graph/cyclepattern/FlightRoutesCheck.java
@@ -0,0 +1,44 @@
+package graph.cyclepattern;
+
+/*
+There are n cities and m flight connections. Your task is to check if you can travel from any city to any other city using the available flights.
+Input
+The first input line has two integers n and m: the number of cities and flights. The cities are numbered 1,2,\dots,n.
+After this, there are m lines describing the flights. Each line has two integers a and b: there is a flight from city a to city b. All flights are one-way flights.
+Output
+Print "YES" if all routes are possible, and "NO" otherwise. In the latter case also print two cities a and b such that you cannot travel from city a to city b. If there are several possible solutions, you can print any of them.
+Constraints
+
+1 \le n \le 10^5
+1 \le m \le 2 \cdot 10^5
+1 \le a,b \le n
+
+Example
+Input:
+4 5
+1 2
+2 3
+3 1
+1 4
+3 4
+
+Output:
+NO
+4 2
+ */
+
+public class FlightRoutesCheck {
+
+    //https://cses.fi/problemset/task/1682
+
+    //todo : implement
+
+    public void isStronlgyConnectedGraph(int n, int[][] routes)
+    {
+        //important : this problem just asks in graph is SCC or not. so simple two DFS approach will work, without going deep into kosaraju/tarjan
+
+        //First DFS on input graph and check if all other nodes reachable or not
+
+        //Second DFS on reversed graph and check if all other nodes reachable or not
+    }
+}
