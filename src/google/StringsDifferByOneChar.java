a6cf52cf4b708570df4ed263b49314d4d1448073
Ramiz Raza <rramiz.rraza@gmail.com>
2026-07-31 10:47:17 +0530

    dsa code refactor

--- src/google/StringsDifferByOneChar.java
+++ src/google/StringsDifferByOneChar.java
@@ -8,14 +8,23 @@ public class StringsDifferByOneChar {
 
     public boolean differByOne(String[] dict)
     {
+        /*
+        // Appraoch:-
+        // Replace each character of every word with a wildcard ('*') one position at a time.
+        // If two words differ by exactly one character at the same position,
+        // they will generate the same wildcard pattern.
+        // Store all generated patterns in a HashSet. If a pattern is seen again,
+        // two words differ by exactly one character.
+         */
+
         Set<String> set = new HashSet<>();
 
         for(String word : dict){
+
             for(int i=0; i<word.length(); i++){
+
                 StringBuilder sb = new StringBuilder();
-                sb.append(word.substring(0, i)).
-                        append("*").
-                        append(word.substring(i+1));
+                sb.append(word.substring(0, i)).append("*").append(word.substring(i+1));
 
                 String pattern = sb.toString();
 
