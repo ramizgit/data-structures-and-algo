6b38d284db133e7545a256de0f903b01f3d6a510
Ramiz Raza <rramiz.rraza@gmail.com>
2026-07-31 11:28:26 +0530

    dsa code refactor

--- src/google/SentenceScreenFitting.java
+++ src/google/SentenceScreenFitting.java
@@ -6,24 +6,35 @@ public class SentenceScreenFitting {
 
     public int wordsTyping(String[] sentence, int rows, int cols)
     {
+        /*
+        Approach:-
+        // Build one cyclic sentence string with spaces.
+        // For each row, greedily consume 'cols' characters.
+        // If we land on a space, start the next row after it.
+        // Otherwise, backtrack to the previous space so that no word is split.
+        // The number of complete sentence fits equals total characters placed / sentence length.
+         */
+
+        //build one full string
         StringBuilder sb = new StringBuilder();
         for(String word : sentence){
             sb.append(word).append(" ");
         }
 
-        String fullString = sb.toString();
-        int len = fullString.length();
+        String fullSentence = sb.toString();
+        int len = fullSentence.length();
 
         int pos = 0;
 
         for(int i=0; i<rows; i++){
-            pos += cols;
 
-            if(fullString.charAt(pos % len) == ' '){
-                pos++;
+            pos += cols; //for each row, greedily consume 'cols' characters
+
+            if(fullSentence.charAt(pos % len) == ' '){
+                pos++; //great, we have landed on a space
             }else{
-                //move back till space
-                while(pos > 0 && fullString.charAt( (pos -1) %len) != ' '){
+                //oops, we landed in the middle of a word, backtrack to the previous space so that no word is split
+                while(pos > 0 && fullSentence.charAt( (pos -1) %len) != ' '){
                     pos--;
                 }
             }
