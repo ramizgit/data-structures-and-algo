9645e19a8ee181e9825163cabb2e4f6c2f4a4f47
Ramiz Raza <rramiz.rraza@gmail.com>
2026-08-01 11:52:27 +0530

    dsa code refactor

--- src/dp/FillingBookCaseShelves.java
+++ src/dp/FillingBookCaseShelves.java
@@ -14,7 +14,7 @@ public class FillingBookCaseShelves {
 
         int n = books.length; //number of books
 
-        int[] dp = new int[n]; // dp[i] = minimum height to place books from 0 to i
+        int[] dp = new int[n]; // dp[i] = minimum height to place books[0...i]
 
         for(int i=0; i<n; i++){
             //for each book try both approach
@@ -24,10 +24,13 @@ public class FillingBookCaseShelves {
             //1. place current book on new shelf
             int currWidth = books[i][0];
             int currHeight = books[i][1];
+
             dp[i] = currHeight  + (i > 0 ? dp[i - 1] : 0);
 
-            //2. try placing previous books on same shelf
+            //2. try extending the last shelf backwards by including previous books
             for(int j=i-1; j>=0; j--){ //since books need to be placed in order
+
+                //try placing books[j...i] on the new shelf
 
                 currWidth += books[j][0];
 
@@ -45,3 +48,44 @@ public class FillingBookCaseShelves {
         return dp[n-1];
     }
 }
+
+/*
+the DP approach :-
+
+"Let's decide where the last shelf begins."
+
+Imagine you're building the final arrangement from scratch.
+
+Suppose you're placing book i.
+
+Candidate 1
+
+The last shelf starts at i.
+
+Previous shelves : books[0...i-1]
+
+Last shelf       : i
+Candidate 2
+
+The last shelf starts at i-1.
+
+Previous shelves : books[0...i-2]
+
+Last shelf       : i-1, i
+Candidate 3
+
+The last shelf starts at i-2.
+
+Previous shelves : books[0...i-3]
+
+Last shelf       : i-2, i-1, i
+Candidate 4
+
+The last shelf starts at i-3.
+
+Previous shelves : books[0...i-4]
+
+Last shelf       : i-3, i-2, i-1, i
+
+and so on...
+ */
