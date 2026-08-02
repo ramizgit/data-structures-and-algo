e8f7317e92c17a424c8e8440b861f4bbc180674e
Ramiz Raza <rramiz.rraza@gmail.com>
2026-08-02 15:33:46 +0530

    dsa code refactor

--- src/grid/dfs/LongestIncreasingPath.java
+++ src/grid/dfs/LongestIncreasingPath.java
@@ -1,24 +1,22 @@
-package grid;
+package grid.dfs;
 
 public class LongestIncreasingPath {
+
     //https://leetcode.com/problems/number-of-increasing-paths-in-a-grid/description/
 
-    public static void main(String[] args)
-    {
-        int[][] grid = { {1, 1}, {3, 4} };
-        System.out.println(countPaths(grid));
-    }
+    private static final int[][] DIRECTIONS = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
 
     private static int countPaths(int[][] grid)
     {
-        if(grid == null){
-            return -1;
+        //input validation
+        if(grid == null || grid.length == 0){
+            return 0;
         }
 
         int m = grid.length;
         int n = grid[0].length;
         int maxLen = 0;
-        int[][] dp = new int[m][n];
+        int[][] dp = new int[m][n]; //dp[i][j] = longest increasing path starting from (i, j)
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
@@ -35,19 +33,21 @@ public class LongestIncreasingPath {
             return dp[i][j];
         }
 
-        int[][] directions = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
         int max = 1; //every cell itself counts
 
-        for(int[] dir : directions){
+        for(int[] dir : DIRECTIONS){
             int x = dir[0] + i;
             int y = dir[1] + j;
 
-            if(x >= 0 && x < m && y >= 0 && y < n && grid[x][y] > grid[i][j]){
+            if(x >= 0 && x < m && y >= 0 && y < n //boundary check
+                    && grid[x][y] > grid[i][j] //constraint check
+            )
+            {
                 max = Math.max(max, 1 + dfs(grid, dp, m, n, x, y));
             }
         }
 
-        dp[i][j] = max;
-        return dp[i][j];
+        dp[i][j] = max; //memoize longest increasing path starting from (i, j)
+        return max;
     }
 }
