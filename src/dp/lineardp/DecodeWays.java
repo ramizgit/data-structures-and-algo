daafcf339302ea2d8a7ee6176356ca285d10c4d7
Ramiz Raza <rramiz.rraza@gmail.com>
2026-08-07 15:44:18 +0530

    dsa code refactor

--- src/dp/lineardp/DecodeWays.java
+++ src/dp/lineardp/DecodeWays.java
@@ -1,4 +1,4 @@
-package consistenthashing.dp.lineardp;
+package dp.lineardp;
 
 public class DecodeWays {
 
@@ -15,18 +15,27 @@ public class DecodeWays {
          */
 
         int n = input.length();
+
+        /*
+        If dp[i] represents an array index, use n; if dp[i] represents a prefix of length i (first i elements/characters), use n + 1.
+         */
+
         int[] dp = new int[n+1]; //dp[i] is the number of ways to decode the first i characters of the string
+
         dp[0] = 1; //base case - empty string has only one way of valid decoding
         dp[1] = input.charAt(0) == '0' ? 0 : 1;
 
         for(int i=2; i<=n; i++){
-            //int oneDigit = Integer.parseInt(input.substring(i-1, i));
+
+            // Choice 1: Decode the last character alone.
+            // Every valid decoding of the first (i-1) characters can be extended by this digit.
             int oneDigit = input.charAt(i - 1) - '0';
             if(oneDigit >= 1){
                 dp[i] += dp[i-1];
             }
 
-            //int twoDigits = Integer.parseInt(input.substring(i-2, i));
+            // Choice 2: Decode the last two characters together.
+            // Every valid decoding of the first (i-2) characters can be extended by this two-digit letter.
             int twoDigits = (input.charAt(i - 2) - '0') * 10 + (input.charAt(i - 1) - '0');
             if(twoDigits >= 10 && twoDigits <= 26){
                 dp[i] += dp[i-2];
