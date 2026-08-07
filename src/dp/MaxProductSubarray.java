7982ad00c84f98ae3536dce2068d8da8a0d4a9b5
Ramiz Raza <rramiz.rraza@gmail.com>
2026-08-07 11:47:24 +0530

    dsa code refactor

--- src/dp/MaxProductSubarray.java
+++ src/dp/MaxProductSubarray.java
@@ -1,43 +1,35 @@
 package dp;
 
 public class MaxProductSubarray {
+
     //https://leetcode.com/problems/maximum-product-subarray/description/
 
-    public static void main(String[] args)
+    public int maxProduct(int[] nums)
     {
-        System.out.println(maxProduct(new int[]{1,2,3})); //6
-        System.out.println(maxProduct(new int[]{1,-2,3})); //3
-        System.out.println(maxProduct(new int[]{-1,-2,3})); //6
-        System.out.println(maxProduct(new int[]{2,3,-2,4})); //6
-        System.out.println(maxProduct(new int[]{-2,0,-1})); //0
-        System.out.println(maxProduct(new int[]{-2,0,1})); //1
-    }
-
-    private static int maxProduct(int[] nums)
-    {
-        int prevMin = nums[0];
-        int prevMax = nums[0];
+        int minEndingHere = nums[0]; //minimum product ending at previous index
+        int maxEndingHere = nums[0]; //maximum product ending at previous index
         int result = nums[0];
 
         for(int i=1; i<nums.length; i++){
-            int currMax = max(nums[i], nums[i] * prevMax, nums[i] * prevMin);
-            int currMin = min(nums[i], nums[i] * prevMax, nums[i] * prevMin);
+
+            int currMax = max(nums[i], nums[i] * maxEndingHere, nums[i] * minEndingHere);
+            int currMin = min(nums[i], nums[i] * maxEndingHere, nums[i] * minEndingHere);
 
             result = Math.max(result, currMax);
 
-            prevMax = currMax;
-            prevMin = currMin;
+            maxEndingHere = currMax;
+            minEndingHere = currMin;
         }
 
         return result;
     }
 
-    private static int max(int a, int b, int c)
+    private int max(int a, int b, int c)
     {
         return Math.max(Math.max(a, b), c);
     }
 
-    private static int min(int a, int b, int c)
+    private int min(int a, int b, int c)
     {
         return Math.min(Math.min(a, b), c);
     }
