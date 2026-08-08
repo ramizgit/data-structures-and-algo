fa5b0cfb980a1ed597f7eff11dd6de90558bcaeb
Ramiz Raza <rramiz.rraza@gmail.com>
2026-08-08 08:37:49 +0530

    dsa code refactor

--- src/array/JumpGameII.java
+++ src/array/JumpGameII.java
@@ -7,15 +7,21 @@ public class JumpGameII {
     public int jump(int[] nums)
     {
         int jumps = 0;
-        int boundary = 0;
+        int boundary = 0; //first jump has to originate from index 0, hence this is starting boundary
         int farthest = 0;
 
         for (int i = 0; i < nums.length - 1; i++) { //why till n-2? we iterate over indices that we might jump from, not indices that we might jump to
 
-            farthest = Math.max(farthest, i + nums[i]); //find the maximum (farthest) while scanning the current range
+            /*
+            Hint:-
+            Within the current reachable range, find the position that gives the farthest next reach.
+            When the current boundary is exhausted, take one jump and move the boundary to that farthest position.
+             */
 
-            // End of current jump range.
+            farthest = Math.max(farthest, i + nums[i]); //find the farthest position reachable from the current jump range
+
             if (i == boundary) {
+                //current jump is exhausted, commit to the next jump
 
                 //important : we would need this check if the problem does not guarantee that you can reach nums[n - 1].
                 /*if (farthest == boundary) {
