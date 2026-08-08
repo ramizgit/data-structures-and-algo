4813ed611e0b21efdd91401f3a10ec2afd8cc009
Ramiz Raza <rramiz.rraza@gmail.com>
2026-08-08 09:27:45 +0530

    dsa code refactor

--- src/greedy/JumpGame.java
+++ src/greedy/JumpGame.java
@@ -1,8 +1,12 @@
-package array;
+package greedy;
 
 public class JumpGame {
 
     //https://leetcode.com/problems/jump-game/description/
+
+    /*
+    Backward greedy: Start with the goal (last index). Find the leftmost position that can reach the goal, then make that position the new goal.
+     */
 
     public boolean canJump(int[] nums)
     {
