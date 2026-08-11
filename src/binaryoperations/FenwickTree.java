4ce684e4d0cfa107b465ca2640aaeb0d770ca6b2
Ramiz Raza <rramiz.rraza@gmail.com>
2026-08-11 20:46:12 +0530

    dsa code FenwickTree

--- src/binaryoperations/FenwickTree.java
+++ src/binaryoperations/FenwickTree.java
@@ -1,5 +1,18 @@
 package binaryoperations;
 
+/*
+Trick : BIT[i] stores the sum of the lowbit(i) elements ending at index i, where lowbit(i) = i & -i.
+that is -
+BIT[1] → 1 element (01)
+BIT[2] → 2 elements (10)
+BIT[3] → 1 element (11)
+BIT[4] → 4 elements (100)
+BIT[5] → 1 element (101)
+BIT[6] → 2 elements (110)
+BIT[7] → 1 element (111)
+BIT[8] → 8 elements (1000)
+ */
+
 public class FenwickTree {
 
     private final int[] bit;
@@ -23,6 +36,8 @@ public class FenwickTree {
     // Returns sum of elements from index 1 to 'index'
     public int query(int index) {
 
+        //get sum of all values from index 1 through index.
+
         int sum = 0;
 
         while (index > 0) {
@@ -34,4 +49,11 @@ public class FenwickTree {
 
         return sum;
     }
+
+    // Returns sum of elements from index left to right, inclusive
+    //rangeQuery(left, right) = query(right) - query(left - 1);
+    public int rangeQuery(int left, int right){
+        return query(right) - query(left - 1);
+    }
+
 }
